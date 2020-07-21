a1158f1b3f7fde3d8f8a49e59ae1a802f25b001d
alters-mit <alters@mit.edu>
2020-07-21 11:54:25 -0400

    Fixed setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 from pathlib import Path
 
-version = Path("tdw/version.py").read_text(encoding="utf-8").split("\n")[0].split(" = ")[1].strip()
+version = "v1.6.0.2"
 
 setup(
     name='tdw',
     version=version,
     description='3D simulation environment',
-    long_description=Path('../README.md').read_text(encoding='utf-8'),
+    long_description=Path('README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
     url='https://github.com/threedworld-mit/tdw',
     download_url=f'https://github.com/threedworld-mit/tdw/archive/v{version}.tar.gz',

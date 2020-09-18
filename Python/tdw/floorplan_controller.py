4177b45b3b0a4d6784558091bcbf659c01c3b9cd
alters <alters@mit.edu>
2020-09-18 12:51:00 -0400

    works

--- Python/tdw/floorplan_controller.py
+++ Python/tdw/floorplan_controller.py
@@ -13,59 +13,60 @@ class FloorplanController(Controller):
     ```python
     from tdw.floorplan_controller import FloorplanController
 
-    c = FloorplanController(scene="2a", layout=0, audio=True)
-    init_commands = c.get_scene_init_commands()
+    c = FloorplanController()
+    init_commands = c.get_scene_init_commands(scene="2a", layout=0, audio=True)
     c.communicate(init_commands)
     ```
 
-    Valid scenes and layouts:
-
-    | `scene` | `layout` |
-    | --- | --- |
-    | 2a | 0 |
-
-    In addition to the fields in `Controller`, there is an `objects` field, a list of initialization data.
-
     """
-    def __init__(self, scene: str, layout: int, audio: bool = False, port: int = 1071, check_version: bool = True,
-                 launch_build: bool = True):
+
+    def get_scene_init_commands(self, scene: str, layout: int, audio: bool) -> List[dict]:
         """
-        :param port: The port number.
-        :param check_version: If true, the controller will check the version of the build and print the result.
-        :param launch_build: If True, automatically launch the build.
+        Get commands to create a scene and populate it with objects.
+
+        Valid scenes and layouts:
+
+        | `scene` | `layout` |
+        | --- | --- |
+        | 2a | 0 |
+
         :param scene: The name of the scene. Corresponds to a record named: `floorplan_[scene]`.
         :param layout: The layout index.
-        :param audio: If True, `objects` will be [`AudioInitData`](object_init_data.md#audioinitdata) and will be assigned physics values based on their audio properties. If False, `objects` will be [`TransformInitData`](object_init_data.md#transforminitdata).
-        """
+        :param audio: If True, i..0nstantiate physics values per object from audio properties.
 
-        self.scene = scene
-        self.layout = str(layout)
-        self.audio = audio
-
-        floorplans = loads(Path(resource_filename(__name__, "floorplan_layouts.json")).
-                           read_text(encoding="utf-8"))
-        if self.scene not in floorplans:
-            raise Exception(f"Floorplan not found: {self.scene}")
-        if self.layout not in floorplans[self.scene]:
-            raise Exception(f"Layout not found: {self.layout}")
-
-        objects = floorplans[self.scene][self.layout]
-        # Deserialize the JSON data.
-        if audio:
-            self.objects = [AudioInitData(**o) for o in objects]
-        else:
-            self.objects = [TransformInitData(**o) for o in objects]
-
-        super().__init__(port=port, check_version=check_version, launch_build=launch_build)
-
-    def get_scene_init_commands(self) -> List[dict]:
-        """
         :return: A list of commands to initialize the scene and populate it with objects.
         """
 
-        commands = [self.get_add_scene(scene_name=f"floorplan_{self.scene}")]
+        scene = scene
+        layout = str(layout)
+
+        floorplans = loads(Path(resource_filename(__name__, "floorplan_layouts.json")).
+                           read_text(encoding="utf-8"))
+        if scene not in floorplans:
+            raise Exception(f"Floorplan not found: {scene}")
+        if layout not in floorplans[scene]:
+            raise Exception(f"Layout not found: {layout}")
+
+        objects = floorplans[scene][layout]
+
+        commands = [self.get_add_scene(scene_name=f"floorplan_{scene}"),
+                    {"$type": "set_aperture",
+                     "aperture": 8.0},
+                    {"$type": "set_focus_distance",
+                     "focus_distance": 2.25},
+                    {"$type": "set_post_exposure",
+                     "post_exposure": 0.4},
+                    {"$type": "set_ambient_occlusion_intensity",
+                     "intensity": 0.175},
+                    {"$type": "set_ambient_occlusion_thickness_modifier",
+                     "thickness": 3.5}]
+        # Deserialize the JSON data.
+        if audio:
+            objects = [AudioInitData(**o) for o in objects]
+        else:
+            objects = [TransformInitData(**o) for o in objects]
         # Get the commands to initialize each object.
-        for o in self.objects:
+        for o in objects:
             object_id, object_commands = o.get_commands()
             commands.extend(object_commands)
         return commands

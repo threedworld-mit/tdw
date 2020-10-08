e227ec8bf8187da5a661e98c6b38acc78a5b7986
alters <alters@mit.edu>
2020-10-08 09:15:40 -0400

    Overlap

--- Python/tdw/FBOutput/Overlap.py
+++ Python/tdw/FBOutput/Overlap.py
@@ -4,28 +4,28 @@
 
 import tdw.flatbuffers
 
-class OverlapSphere(object):
+class Overlap(object):
     __slots__ = ['_tab']
 
     @classmethod
-    def GetRootAsOverlapSphere(cls, buf, offset):
+    def GetRootAsOverlap(cls, buf, offset):
         n = tdw.flatbuffers.encode.Get(tdw.flatbuffers.packer.uoffset, buf, offset)
-        x = OverlapSphere()
+        x = Overlap()
         x.Init(buf, n + offset)
         return x
 
-    # OverlapSphere
+    # Overlap
     def Init(self, buf, pos):
         self._tab = tdw.flatbuffers.table.Table(buf, pos)
 
-    # OverlapSphere
+    # Overlap
     def Id(self):
         o = tdw.flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(4))
         if o != 0:
             return self._tab.Get(tdw.flatbuffers.number_types.Int32Flags, o + self._tab.Pos)
         return 0
 
-    # OverlapSphere
+    # Overlap
     def ObjectIds(self, j):
         o = tdw.flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
         if o != 0:
@@ -33,30 +33,30 @@ class OverlapSphere(object):
             return self._tab.Get(tdw.flatbuffers.number_types.Int32Flags, a + tdw.flatbuffers.number_types.UOffsetTFlags.py_type(j * 4))
         return 0
 
-    # OverlapSphere
+    # Overlap
     def ObjectIdsAsNumpy(self):
         o = tdw.flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
         if o != 0:
             return self._tab.GetVectorAsNumpy(tdw.flatbuffers.number_types.Int32Flags, o)
         return 0
 
-    # OverlapSphere
+    # Overlap
     def ObjectIdsLength(self):
         o = tdw.flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
         if o != 0:
             return self._tab.VectorLen(o)
         return 0
 
-    # OverlapSphere
+    # Overlap
     def Env(self):
         o = tdw.flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
         if o != 0:
             return bool(self._tab.Get(tdw.flatbuffers.number_types.BoolFlags, o + self._tab.Pos))
         return False
 
-def OverlapSphereStart(builder): builder.StartObject(3)
-def OverlapSphereAddId(builder, id): builder.PrependInt32Slot(0, id, 0)
-def OverlapSphereAddObjectIds(builder, objectIds): builder.PrependUOffsetTRelativeSlot(1, tdw.flatbuffers.number_types.UOffsetTFlags.py_type(objectIds), 0)
-def OverlapSphereStartObjectIdsVector(builder, numElems): return builder.StartVector(4, numElems, 4)
-def OverlapSphereAddEnv(builder, env): builder.PrependBoolSlot(2, env, 0)
-def OverlapSphereEnd(builder): return builder.EndObject()
+def OverlapStart(builder): builder.StartObject(3)
+def OverlapAddId(builder, id): builder.PrependInt32Slot(0, id, 0)
+def OverlapAddObjectIds(builder, objectIds): builder.PrependUOffsetTRelativeSlot(1, tdw.flatbuffers.number_types.UOffsetTFlags.py_type(objectIds), 0)
+def OverlapStartObjectIdsVector(builder, numElems): return builder.StartVector(4, numElems, 4)
+def OverlapAddEnv(builder, env): builder.PrependBoolSlot(2, env, 0)
+def OverlapEnd(builder): return builder.EndObject()

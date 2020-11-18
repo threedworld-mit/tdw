58a3b4335ae665e835dbac560f92f0da92363e81
alters-mit <>
2020-11-18 11:13:23 -0500

    fetch

--- Python/tdw/librarian.py
+++ Python/tdw/librarian.py
@@ -183,6 +183,7 @@ class RobotRecord(_Record):
         super().__init__(data)
         self.source = data["source"]
         self.immovable = data["immovable"]
+        self.camera = data["camera"]
 
 
 T = TypeVar("T", bound=_Record)

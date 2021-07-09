ebed1f1444f04678c45d8de3625eef5c81eb5edb
Seth Alter <alters@mit.edu>
2021-07-09 15:06:16 -0400

    works!

--- Python/tdw/add_ons/robot.py
+++ Python/tdw/add_ons/robot.py
@@ -8,9 +8,53 @@ from tdw.add_ons.robots.robot_dynamic import RobotDynamic
 
 
 class Robot(AddOn):
-    ROBOT_LIBRARIAN: RobotLibrarian = RobotLibrarian()
+    """
+    Add a robot to the scene and control its joints.
 
-    def __init__(self, robot: str, position: Dict[str, float] = None, rotation: Dict[str, float] = None,
+    ```python
+    from tdw.controller import Controller
+    from tdw.tdw_utils import TDWUtils
+    from tdw.add_ons.robot import Robot
+    from tdw.add_ons.third_person_camera import ThirdPersonCamera
+
+    c = Controller(launch_build=False)
+    c.start()
+    # Create the robot add-on.
+    robot_id = 0
+    robot = Robot(name="ur5",
+                  position={"x": 1, "y": 0, "z": -0.5},
+                  robot_id=robot_id)
+    # Create a camera add-on (for the purposes of seeing what's actually happening in this example).
+    camera = ThirdPersonCamera(position={"x": -0.5, "y": 1, "z": 0},
+                               look_at=robot_id)
+    c.add_ons.extend([robot, camera])
+
+    # Create an empty room (this will add the robot and the camera).
+    c.communicate(TDWUtils.create_empty_room(12, 12))
+
+    # Set target angles for the shoulder and forearm.
+    targets = dict()
+    for joint_id in robot.static.joints:
+        name = robot.static.joints[joint_id].name
+        if name == "shoulder_link":
+            targets[joint_id] = 70
+        elif name == "forearm_link":
+            targets[joint_id] = -45
+    robot.set_joint_targets(targets=targets)
+
+    # Wait for the shoulder and forearm to stop moving.
+    while robot.joints_are_moving():
+        c.communicate([])
+
+    # End the simulation.
+    c.communicate({"$type": "terminate"})
+    ```
+    """
+
+    ROBOT_LIBRARIAN: RobotLibrarian = RobotLibrarian()
+    NON_MOVING: float = 0.001
+
+    def __init__(self, name: str, position: Dict[str, float] = None, rotation: Dict[str, float] = None,
                  robot_id: int = 0, source: Union[RobotLibrarian, RobotRecord, str] = None):
         """
         :param robot_id: The ID of the robot.
@@ -32,15 +76,16 @@ class Robot(AddOn):
 
         url: str
         if source is None:
-            url = Robot.ROBOT_LIBRARIAN.get_record(robot).get_url()
+            url = Robot.ROBOT_LIBRARIAN.get_record(name).get_url()
         elif isinstance(source, RobotLibrarian):
-            url = source.get_record(robot).get_url()
+            url = source.get_record(name).get_url()
         elif isinstance(source, RobotRecord):
             url = source.get_url()
         elif isinstance(source, str):
             url = source
         else:
             raise TypeError("Invalid type for source: " + source)
+
         if position is None:
             position = {"x": 0, "y": 0, "z": 0}
         if rotation is None:
@@ -50,7 +95,7 @@ class Robot(AddOn):
                                          "id": robot_id,
                                          "position": position,
                                          "rotation": rotation,
-                                         "name": robot,
+                                         "name": name,
                                          "url": url}
 
     def get_initialization_commands(self) -> List[dict]:
@@ -64,33 +109,77 @@ class Robot(AddOn):
         # Initialize the static data.
         if self.static is None:
             self.static = RobotStatic(resp=resp, robot_id=self.robot_id)
-        self.dynamic = RobotDynamic(resp=resp, robot_id=self.robot_id, previous=self.dynamic)
+        dynamic = RobotDynamic(resp=resp, robot_id=self.robot_id, previous=self.dynamic)
+        if self.dynamic is not None:
+            # Check which joints are still moving.
+            for joint_id in dynamic.joints:
+                dynamic.joints[joint_id].moving = False
+                for angle_0, angle_1 in zip(self.dynamic.joints[joint_id].angles, dynamic.joints[joint_id].angles):
+                    if np.linalg.norm(angle_1 - angle_0) > Robot.NON_MOVING:
+                        dynamic.joints[joint_id].moving = True
+                        break
+        else:
+            for joint_id in dynamic.joints:
+                dynamic.joints[joint_id].moving = True
+        # Update the dynamic info.
+        self.dynamic = dynamic
 
-    def set_joint_targets(self, joints: Dict[int, Union[float, Dict[str, float]]]) -> None:
+    def set_joint_targets(self, targets: Dict[int, Union[float, Dict[str, float]]]) -> None:
         """
         Set target angles or positions for a dictionary of joints.
 
-        :param joints: A dictionary of joints to set. Key = The ID of the joint. Value = the targets. For spherical joints, this must be a Vector3 dictionary, for example `{"x": 40, "y": 0, "z": 0}`. For revolute and prismatic joints, this must be a float.
+        :param targets: A dictionary of joint targets. Key = The ID of the joint. Value = the targets. For spherical joints, this must be a Vector3 dictionary, for example `{"x": 40, "y": 0, "z": 0}` (angles in degrees). For revolute joints, this must be a float (an angle in degrees). For prismatic joints, this must be a float (a distance in meters).
         """
 
-        for joint_id in joints:
+        for joint_id in targets:
             joint_type = self.static.joints[joint_id].joint_type
-            if joint_type == "fixed":
-                continue
-            elif joint_type == "spherical":
-                assert isinstance(joints[joint_id], dict), f"Expected an x, y, z dictionary of targets for " \
-                                                           f"{joint_id} but got {joints[joint_id]}"
+            if joint_type == "spherical":
                 self.commands.append({"$type": "set_spherical_target",
-                                      "target": joints[joint_id],
+                                      "target": targets[joint_id],
+                                      "joint_id": joint_id,
+                                      "id": self.robot_id})
+            elif joint_type == "revolute":
+                self.commands.append(
+                    {"$type": "set_revolute_target",
+                     "target": float(targets[joint_id]),
+                     "joint_id": joint_id,
+                     "id": self.robot_id})
+            elif joint_type == "prismatic":
+                self.commands.append(
+                    {"$type": "set_prismatic_target",
+                     "target": float(targets[joint_id]),
+                     "joint_id": joint_id,
+                     "id": self.robot_id})
+            else:
+                raise Exception(f"Cannot set target for joint type {joint_type}")
+
+    def add_joint_forces(self, forces: Dict[int, Union[float, Dict[str, float]]]) -> None:
+
+        """
+        Add torques and forces to a dictionary of joints.
+
+        :param forces: A dictionary of joint forces. Key = The ID of the joint. Value = the targets. For spherical joints, this must be a Vector3 dictionary, for example `{"x": 40, "y": 0, "z": 0}` (torques in Newtons). For revolute joints, this must be a float (a torque in Newtons). For prismatic joints, this must be a float (a force in Newtons).
+        """
+
+        for joint_id in forces:
+            joint_type = self.static.joints[joint_id].joint_type
+            if joint_type == "spherical":
+                self.commands.append({"$type": "add_torque_to_spherical",
+                                      "torque": forces[joint_id],
+                                      "joint_id": joint_id,
+                                      "id": self.robot_id})
+            elif joint_type == "revolute":
+                self.commands.append({"$type": "add_torque_to_revolute",
+                                      "torque": float(forces[joint_id]),
+                                      "joint_id": joint_id,
+                                      "id": self.robot_id})
+            elif joint_type == "prismatic":
+                self.commands.append({"$type": "add_force_to_prismatic",
+                                      "force": float(forces[joint_id]),
                                       "joint_id": joint_id,
                                       "id": self.robot_id})
             else:
-                assert isinstance(joints[joint_id], float), f"Expected a float target for " \
-                                                            f"{joint_id} but got {joints[joint_id]}"
-                self.commands.append({"$type": "set_revolute_target" if joint_type == "revolute" else "set_prismatic_target",
-                                      "target": float(joints[joint_id]),
-                                      "joint_id": joint_id,
-                                      "id": self.robot_id})
+                raise Exception(f"Cannot apply torque or force to joint type {joint_type}")
 
     def stop_joints(self, joint_ids: List[int] = None) -> None:
         """
@@ -134,7 +223,7 @@ class Robot(AddOn):
         """
 
         if joint_ids is None:
-            joint_ids = self.dynamic.joints.values()
+            joint_ids = self.dynamic.joints.keys()
         for joint_id in joint_ids:
             if self.dynamic.joints[joint_id].moving:
                 return True

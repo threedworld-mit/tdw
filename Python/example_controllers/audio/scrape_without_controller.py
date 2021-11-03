f9a8d27862fd7e36db44a2bb1338496556716465
Seth Alter <alters@mit.edu>
2021-11-03 15:40:44 -0400

    fixed example controllers

--- Python/example_controllers/audio/scrape_without_controller.py
+++ Python/example_controllers/audio/scrape_without_controller.py
@@ -20,7 +20,7 @@ for i in range(3):
 for i in range(5):
     sound: Optional[Base64Sound] = None
     for j in range(5):
-        s = py_impact.get_scrape_sound(velocity=np.array([4, 0, 0]),
+        s = py_impact.get_scrape_sound(velocity=np.array([1.5, 0, 0]),
                                        contact_normals=contact_normals,
                                        primary_id=0,
                                        primary_material="metal_1",
@@ -34,7 +34,7 @@ for i in range(5):
                                        scrape_material=ScrapeMaterial.ceramic)
         if sound is None:
             sound = s
-        else:
+        elif s is not None:
             sound.bytes += s.bytes
             sound.length += s.length
     sound.write(path=output_directory.joinpath(f"{i}.wav"))

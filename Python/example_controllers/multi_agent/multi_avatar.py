c179bee7bbba76cdd0b0b7271b5b2cb3a88a3d57
Seth Alter <alters@mit.edu>
2021-11-23 10:10:24 -0500

    wip

--- Python/example_controllers/multi_agent/multi_avatar.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-from typing import Dict, Union, List
-from tdw.controller import Controller
-from tdw.tdw_utils import TDWUtils
-from tdw.add_ons.embodied_avatar import EmbodiedAvatar
-from tdw.add_ons.third_person_camera import ThirdPersonCamera
-from tdw.add_ons.image_capture import ImageCapture
-from tdw.backend.paths import EXAMPLE_CONTROLLER_OUTPUT_PATH

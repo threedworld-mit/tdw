68e22f1d3d5bd267c7f966152e9b163e35b40fc2
Seth Alter <alters@mit.edu>
2022-02-25 10:22:57 -0500

    refrigerator

--- Python/tdw/proc_gen/arrangements/refrigerator.py
+++ Python/tdw/proc_gen/arrangements/refrigerator.py
@@ -1,5 +1,7 @@
+from pathlib import Path
+from pkg_resources import resource_filename
+from json import loads
 from typing import List
-from tdw.tdw_utils import TDWUtils
 from tdw.proc_gen.arrangements.arrangement_along_wall import ArrangementAlongWall
 
 
@@ -11,21 +13,22 @@ class Refrigerator(ArrangementAlongWall):
     - The refrigerator is placed next to a wall.
       - The refrigerator's position is automatically adjusted to set it flush to the way.
       - The refrigerator is automatically rotated so that it faces away from the wall.
-    - The refrigerator's rotation is random.
     - The refrigerator is non-kinematic.
     """
+
+    _ROTATIONS = loads(Path(resource_filename(__name__, "data/refrigerators.json")).read_text())
 
     def get_commands(self) -> List[dict]:
         return self._add_root_object()
 
     def get_length(self) -> float:
-        return TDWUtils.get_bounds_extents(bounds=self._record.bounds)[0]
+        return Refrigerator._ROTATIONS[self._record.name]["length"]
 
     def _get_depth(self) -> float:
-        return TDWUtils.get_bounds_extents(bounds=self._record.bounds)[2]
+        return Refrigerator._ROTATIONS[self._record.name]["depth"]
 
     def _get_category(self) -> str:
         return "refrigerator"
 
     def _get_rotation(self) -> float:
-        return self._rng.uniform(0, 360)
+        return Refrigerator._ROTATIONS[self._record.name]["rotations"][self._wall.name]

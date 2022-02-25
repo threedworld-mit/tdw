ba8dcb06693df9364cd098cc8d90d90273e3e2b4
Seth Alter <alters@mit.edu>
2022-02-25 10:33:13 -0500

    Radiator

---
+++ Python/tdw/proc_gen/arrangements/radiator.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from pkg_resources import resource_filename
+from json import loads
+from typing import List
+from tdw.proc_gen.arrangements.arrangement_along_wall import ArrangementAlongWall
+
+
+class Radiator(ArrangementAlongWall):
+    """
+    A radiator.
+
+    - The radiator model is chosen randomly; see `Radiator.MODEL_CATEGORIES["radiator"]`.
+    - The radiator is placed next to a wall.
+      - The radiator's position is automatically adjusted to set it flush to the way.
+      - The radiator is automatically rotated so that it faces away from the wall.
+    - The radiator is non-kinematic.
+    """
+
+    _ROTATIONS = loads(Path(resource_filename(__name__, "data/radiators.json")).read_text())
+
+    def get_commands(self) -> List[dict]:
+        return self._add_root_object()
+
+    def get_length(self) -> float:
+        return Radiator._ROTATIONS[self._record.name]["length"]
+
+    def _get_depth(self) -> float:
+        return Radiator._ROTATIONS[self._record.name]["depth"] + Radiator._ROTATIONS[self._record.name]["depth_offset"]
+
+    def _get_category(self) -> str:
+        return "radiator"
+
+    def _get_rotation(self) -> float:
+        return Radiator._ROTATIONS[self._record.name]["rotations"][self._wall.name]

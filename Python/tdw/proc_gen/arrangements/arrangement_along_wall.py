d1bd539ad7e68de2840be232bd0fb0408fd622a0
Seth Alter <alters@mit.edu>
2022-03-02 09:08:53 -0500

    hmm

--- Python/tdw/proc_gen/arrangements/arrangement_along_wall.py
+++ Python/tdw/proc_gen/arrangements/arrangement_along_wall.py
@@ -2,7 +2,7 @@ from typing import List, Dict, Union, Optional
 from abc import ABC, abstractmethod
 import numpy as np
 from tdw.controller import Controller
-from tdw.proc_gen.proc_gen_utils import ProcGenUtils
+from tdw.tdw_utils import TDWUtils
 from tdw.proc_gen.arrangements.arrangement_with_root_object import ArrangementWithRootObject
 from tdw.scene_data.interior_region import InteriorRegion
 from tdw.cardinal_direction import CardinalDirection
@@ -29,7 +29,7 @@ class ArrangementAlongWall(ArrangementWithRootObject, ABC):
 
         self._corner: OrdinalDirection = corner
         self._wall: CardinalDirection = wall
-        self._direction: CardinalDirection = ProcGenUtils.get_direction_from_corner(corner=corner, wall=self._wall)
+        self._direction: CardinalDirection = TDWUtils.get_direction_from_corner(corner=corner, wall=self._wall)
         self._distance: float = distance
         self._region: InteriorRegion = region
         if wall_length is None:
@@ -76,9 +76,7 @@ class ArrangementAlongWall(ArrangementWithRootObject, ABC):
         :return: The adjusted position along the wall.
         """
 
-        return ProcGenUtils.get_position_along_wall(length=self.get_length(), depth=self._get_depth(),
-                                                    corner=self._corner, wall=self._wall, distance=self._distance,
-                                                    region=self._region)
+        return self._get_position_along_wall()
 
     def _get_model_names(self) -> List[str]:
         """
@@ -109,3 +107,61 @@ class ArrangementAlongWall(ArrangementWithRootObject, ABC):
         """
 
         raise Exception()
+
+    def _get_position_along_wall(self) -> Dict[str, float]:
+        """
+        :return: The position along the `wall` in the `region` at `distance` meters from the `corner`.
+        """
+
+        depth = self._get_depth()
+        length = self.get_length()
+        # Get the position at the origin corner plus the depth offset.
+        if self._wall == CardinalDirection.north:
+            z = self._region.z_max - depth / 2
+            if self._corner == OrdinalDirection.northeast:
+                x = self._region.x_max
+            elif self._corner == OrdinalDirection.northwest:
+                x = self._region.x_min
+            else:
+                raise Exception(f"Invalid corner: {self._corner}")
+        elif self._wall == CardinalDirection.south:
+            z = self._region.z_min + depth / 2
+            if self._corner == OrdinalDirection.southeast:
+                x = self._region.x_max
+            elif self._corner == OrdinalDirection.southwest:
+                x = self._region.x_min
+            else:
+                raise Exception(f"Invalid corner: {self._corner}")
+        elif self._wall == CardinalDirection.west:
+            x = self._region.x_min + depth / 2
+            if self._corner == OrdinalDirection.northwest:
+                z = self._region.z_max
+            elif self._corner == OrdinalDirection.southwest:
+                z = self._region.z_min
+            else:
+                raise Exception(f"Invalid corner: {self._corner}")
+        elif self._wall == CardinalDirection.east:
+            x = self._region.x_max - depth / 2
+            if self._corner == OrdinalDirection.northeast:
+                z = self._region.z_max
+            elif self._corner == OrdinalDirection.southeast:
+                z = self._region.z_min
+            else:
+                raise Exception(f"Invalid corner: {self._corner}")
+        else:
+            raise Exception(self._wall)
+        # Get the distance offset.
+        pos = {"x": x, "y": 0, "z": z}
+        distance = self._distance + length / 2
+        direction = TDWUtils.get_direction_from_corner(corner=self._corner, wall=self._wall)
+        if direction == CardinalDirection.north:
+            pos["z"] += distance
+        elif direction == CardinalDirection.south:
+            pos["z"] -= distance
+        elif direction == CardinalDirection.west:
+            pos["x"] -= distance
+        elif direction == CardinalDirection.east:
+            pos["x"] += distance
+        else:
+            raise Exception(direction)
+        return pos
--- Python/tdw/proc_gen/proc_gen_utils.py
+++ Python/tdw/proc_gen/proc_gen_utils.py
@@ -9,127 +9,13 @@ from tdw.cardinal_direction import CardinalDirection
 from tdw.ordinal_direction import OrdinalDirection
 from tdw.scene_data.room import Room
 from tdw.scene_data.interior_region import InteriorRegion
+from tdw.proc_gen.arrangements.arrangement_along_wall import ArrangementAlongWall
 
 
 class ProcGenUtils:
     """
     Utility class for procedural generation.
     """
-
-    @staticmethod
-    def get_corners_from_wall(wall: CardinalDirection) -> List[OrdinalDirection]:
-        """
-        :param wall: The wall as a [`CardinalDirection`](../cardinal_direction.md).
-
-        :return: The corners of the wall as a 2-element list of [`OrdinalDirection`](../ordinal_direction.md).
-        """
-
-        if wall == CardinalDirection.north:
-            return [OrdinalDirection.northwest, OrdinalDirection.northeast]
-        elif wall == CardinalDirection.south:
-            return [OrdinalDirection.southwest, OrdinalDirection.southeast]
-        elif wall == CardinalDirection.west:
-            return [OrdinalDirection.northwest, OrdinalDirection.southwest]
-        elif wall == CardinalDirection.east:
-            return [OrdinalDirection.northeast, OrdinalDirection.southeast]
-
-    @staticmethod
-    def get_direction_from_corner(corner: OrdinalDirection, wall: CardinalDirection) -> CardinalDirection:
-        """
-        Given an corner an a wall, get the direction that a lateral arrangement will run along.
-
-        :param corner: The corner as an [`OrdinalDirection`](../ordinal_direction.md).
-        :param wall: The wall as a [`CardinalDirection`](../cardinal_direction.md).
-
-        :return: Tuple: direction, wall
-        """
-
-        if corner == OrdinalDirection.northwest:
-            if wall == CardinalDirection.north:
-                return CardinalDirection.east
-            elif wall == CardinalDirection.west:
-                return CardinalDirection.south
-        elif corner == OrdinalDirection.northeast:
-            if wall == CardinalDirection.north:
-                return CardinalDirection.west
-            elif wall == CardinalDirection.east:
-                return CardinalDirection.south
-        elif corner == OrdinalDirection.southwest:
-            if wall == CardinalDirection.south:
-                return CardinalDirection.east
-            elif wall == CardinalDirection.west:
-                return CardinalDirection.north
-        elif corner == OrdinalDirection.southeast:
-            if wall == CardinalDirection.south:
-                return CardinalDirection.west
-            elif wall == CardinalDirection.east:
-                return CardinalDirection.north
-        raise Exception(corner, wall)
-
-    @staticmethod
-    def get_position_along_wall(length: float, depth: float, corner: OrdinalDirection, wall: CardinalDirection,
-                                distance: float, region: InteriorRegion) -> Dict[str, float]:
-        """
-        :param length: The length of the object in meters.
-        :param depth: The depth of the object in meters.
-        :param corner: The corner as an [`OrdinalDirection`](../ordinal_direction.md).
-        :param wall: The wall as a [`CardinalDirection`](../cardinal_direction.md).
-        :param distance: The distance from the corner along the wall.
-        :param region: The [`InteriorRegion`](../scene_data/interior_region.md).
-
-        :return: The position along the `wall` in the `region` at `distance` meters from the `corner`.
-        """
-
-        # Get the position at the origin corner plus the depth offset.
-        if wall == CardinalDirection.north:
-            z = region.z_max - depth / 2
-            if corner == OrdinalDirection.northeast:
-                x = region.x_max
-            elif corner == OrdinalDirection.northwest:
-                x = region.x_min
-            else:
-                raise Exception(f"Invalid corner: {corner}")
-        elif wall == CardinalDirection.south:
-            z = region.z_min + depth / 2
-            if corner == OrdinalDirection.southeast:
-                x = region.x_max
-            elif corner == OrdinalDirection.southwest:
-                x = region.x_min
-            else:
-                raise Exception(f"Invalid corner: {corner}")
-        elif wall == CardinalDirection.west:
-            x = region.x_min + depth / 2
-            if corner == OrdinalDirection.northwest:
-                z = region.z_max
-            elif corner == OrdinalDirection.southwest:
-                z = region.z_min
-            else:
-                raise Exception(f"Invalid corner: {corner}")
-        elif wall == CardinalDirection.east:
-            x = region.x_max - depth / 2
-            if corner == OrdinalDirection.northeast:
-                z = region.z_max
-            elif corner == OrdinalDirection.southeast:
-                z = region.z_min
-            else:
-                raise Exception(f"Invalid corner: {corner}")
-        else:
-            raise Exception(wall)
-        # Get the distance offset.
-        pos = {"x": x, "y": 0, "z": z}
-        distance = distance + length / 2
-        direction = ProcGenUtils.get_direction_from_corner(corner=corner, wall=wall)
-        if direction == CardinalDirection.north:
-            pos["z"] += distance
-        elif direction == CardinalDirection.south:
-            pos["z"] -= distance
-        elif direction == CardinalDirection.west:
-            pos["x"] -= distance
-        elif direction == CardinalDirection.east:
-            pos["x"] += distance
-        else:
-            raise Exception(direction)
-        return pos
 
     @staticmethod
     def get_proc_gen_box_room(width: int, length: int) -> Tuple[List[dict], Room]:
@@ -154,12 +40,15 @@ class ProcGenUtils:
         raise Exception(f"Room size not found: {width}, {length}")
 
     @staticmethod
-    def get_lateral_arrangement(categories: List[str], corner: OrdinalDirection, wall: CardinalDirection,
+    def get_lateral_arrangement(arrangements: List[ArrangementAlongWall], corner: OrdinalDirection, wall: CardinalDirection,
                                 region: InteriorRegion, length: float = None, rng: np.random.RandomState = None) -> List[dict]:
         if length is None:
             length = region.get_length(side=wall)
         distance = 0
         commands = []
+        for arrangement in arrangements:
+            commands.extend(arrangement.get_commands())
+        return commands
         for category in categories:
             # A very dirty hack.
             klass = getattr(importlib.import_module(f"tdw.proc_gen.arrangements.{category}"),

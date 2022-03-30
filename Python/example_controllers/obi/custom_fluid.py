a381613e2bc4e00d82967c70636d6df5958f9d39
Seth Alter <alters@mit.edu>
2022-03-30 09:27:39 -0400

    updated custom_fluid.py

--- Python/example_controllers/obi/custom_fluid.py
+++ Python/example_controllers/obi/custom_fluid.py
@@ -11,29 +11,43 @@ Add a custom fluid to the scene.
 c = Controller()
 c.communicate(Controller.get_add_scene(scene_name="tdw_room"))
 fluid_id = Controller.get_unique_id()
-camera = ThirdPersonCamera(position={"x": 1.2, "y": 1.5, "z": -1.5},
+camera = ThirdPersonCamera(position={"x": -3.75, "y": 1.5, "z": -0.5},
                            look_at={"x": 0, "y": 0, "z": 0})
 obi = Obi()
 c.add_ons.extend([camera, obi])
-fluid = Fluid(capacity=1500,
-              resolution=1,
-              color={"r": 0, "g": 1, "b": 0.5, "a": 0.8},
+# Define a custom fluid.
+fluid = Fluid(capacity=1000,
+              resolution=0.5,
+              color={"r": 0.8, "g": 0.8, "b": 0.8, "a": 1.0},
               rest_density=1000,
-              reflection=0.25,
-              refraction=0.034,
-              smoothing=1.8,
-              viscosity=0.001,
-              vorticity=1,
-              surface_tension=1,
-              transparency=1)
+              reflection=0.95,
+              refraction=0,
+              smoothing=3.0,
+              render_smoothness=1.0,
+              metalness=0.1,
+              viscosity=0.5,
+              absorption=0,
+              vorticity=0,
+              surface_tension=2.0,
+              transparency=0.15,
+              thickness_cutoff=2,
+              radius_scale=1.5,
+              random_velocity=0.125)
+# Create the fluid.
 obi.create_fluid(fluid=fluid,
                  shape=CubeEmitter(),
                  object_id=fluid_id,
-                 position={"x": 0, "y": 2.35, "z": 0},
+                 position={"x": -0.1, "y": 2.0, "z": 0},
                  rotation={"x": 90, "y": 0, "z": 0},
-                 speed=1)
-c.communicate(Controller.get_add_physics_object(model_name="rh10",
-                                                object_id=Controller.get_unique_id()))
-for i in range(200):
+                 lifespan=6,
+                 speed=0.75)
+# Add an object for the fluid to interact with.
+c.communicate(Controller.get_add_physics_object(model_name="sphere",
+                                                object_id=Controller.get_unique_id(),
+                                                library="models_flex.json",
+                                                kinematic=True,
+                                                gravity=False,
+                                                scale_factor={"x": 0.5, "y": 0.5, "z": 0.5}))
+for i in range(500):
     c.communicate([])
 c.communicate({"$type": "terminate"})

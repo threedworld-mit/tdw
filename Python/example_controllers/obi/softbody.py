5bd9acac337a8567bf89553bda74356454ef6f31
instbldrjems <jeremyes@mit.edu>
2022-05-18 12:34:35 -0400

    material and controller tweaks.

--- Python/example_controllers/obi/softbody.py
+++ Python/example_controllers/obi/softbody.py
@@ -8,20 +8,25 @@ Minimal example of dropping a cloth sheet onto an object.
 """
 
 c = Controller(launch_build=False)
-camera = ThirdPersonCamera(position={"x": -3.75, "y": 1.5, "z": -0.5},
+camera = ThirdPersonCamera(position={"x": -5.0, "y": 0.8, "z": -0.5},
                            look_at={"x": 0, "y": 1.25, "z": 0})
 obi = Obi()
 c.add_ons.extend([camera, obi])
-# Increase the solver substeps to accommodate the high viscosity and smoothing and make the fluid behave more like jam.
+# Increase the solver substeps.
 obi.set_solver(substeps=2)
 # Create a softbody object.
 obi.create_softbody(softbody_material="hard_rubber",
                     object_id=Controller.get_unique_id(),
-                    position={"x": 0, "y": 2, "z": 0},
+                    position={"x": 0, "y": 2, "z": 1},
+                    rotation={"x": 0, "y": 0, "z": 0})
+# Create a second softbody object.
+obi.create_softbody(softbody_material="soft_rubber",
+                    object_id=Controller.get_unique_id(),
+                    position={"x": 0, "y": 2, "z": -1},
                     rotation={"x": 0, "y": 0, "z": 0})
 commands = [TDWUtils.create_empty_room(12, 12)]
 c.communicate(commands)
-# Let the cloth fall.
+# Let the object fall.
 while True:
     c.communicate([])
 #c.communicate({"$type": "terminate"})

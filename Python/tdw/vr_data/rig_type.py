0d5740d0d95b73134698792033a3fb4c207108c6
jemsMIT <jeremyes@mit.edu>
2022-06-01 09:07:44 -0400

    Initial commit -- rig type and prototype add-on

--- Python/tdw/vr_data/rig_type.py
+++ Python/tdw/vr_data/rig_type.py
@@ -8,3 +8,4 @@ class RigType(Enum):
 
     oculus_touch_robot_hands = 1  # Oculus Touch controller. Hands are rendered as robot hands.
     oculus_touch_human_hands = 2  # Oculus Touch controller. Hands are rendered as human hands.
+    leap_motion_capsule_hands = 3  # Leap Motion controller. Hands are rendered as capsule hands.

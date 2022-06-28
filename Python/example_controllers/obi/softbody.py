10db243cae82849377c0aaa167b79807f6edb3f5
instbldrjems <jeremyes@mit.edu>
2022-06-28 15:41:26 -0400

    Testing materials

--- Python/example_controllers/obi/softbody.py
+++ Python/example_controllers/obi/softbody.py
@@ -8,26 +8,35 @@ Minimal example of dropping a cloth sheet onto an object.
 """
 
 c = Controller(launch_build=False)
-camera = ThirdPersonCamera(position={"x": -5.0, "y": 0.8, "z": -0.5},
-                           look_at={"x": 0, "y": 1.25, "z": 0})
+camera = ThirdPersonCamera(position={"x": -5.0, "y": 0.8, "z": -1},
+                           look_at={"x": 0, "y": 1, "z": 0})
 obi = Obi()
 c.add_ons.extend([camera, obi])
 c.communicate([TDWUtils.create_empty_room(12, 12)])
 c.communicate([{"$type": "create_obi_solver"}])
 obi.set_solver(solver_id=0, substeps=2)
 obi.set_solver(solver_id=1, substeps=2)
-# Create a softbody object, using the first solver vreated by the add-on.
+c.communicate([{"$type": "create_obi_solver"}])
+obi.set_solver(solver_id=2, substeps=2)
+# Create a softbody object, using the first solver created by the add-on.
 obi.create_softbody(softbody_material="hard_rubber",
                     object_id=Controller.get_unique_id(),
-                    position={"x": 0, "y": 2, "z": 1},
-                    rotation={"x": 0, "y": 0, "z": 0},
+                    position={"x": 0, "y": 3, "z": 2},
+                    rotation={"x": -10, "y": 0, "z": 10},
                     solver_id=0)
-# Create a second softbody object with a different material, using the second solver.
-obi.create_softbody(softbody_material="firm_foam",
+
+obi.create_softbody(softbody_material="water_balloon",
                     object_id=Controller.get_unique_id(),
-                    position={"x": 0, "y": 2, "z": -1},
-                    rotation={"x": 0, "y": 0, "z": 0},
+                    position={"x": 0, "y": 3, "z": 0},
+                    rotation={"x": 10, "y": 0, "z": 10},
                     solver_id=1)
+# Create a second softbody object with a different material, using the second solver.
+
+obi.create_softbody(softbody_material="soft_foam",
+                    object_id=Controller.get_unique_id(),
+                    position={"x": 0, "y": 3, "z": -2},
+                    rotation={"x": 15, "y": 0, "z": -15},
+                    solver_id=2)
 
 # Let the object fall.
 while True:

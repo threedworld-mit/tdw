d2107c6b47f564cfe6ac47780394571ea78b73de
instbldrjems <jeremyes@mit.edu>
2022-10-04 13:08:44 -0400

    Update leap_motion_minimal.py

--- Python/example_controllers/vr/leap_motion_minimal.py
+++ Python/example_controllers/vr/leap_motion_minimal.py
@@ -9,9 +9,16 @@ Minimal Oculus touch example.
 c = Controller(launch_build=False)
 vr = LeapMotion()
 c.add_ons.append(vr)
+obj_id = Controller.get_unique_id()
 c.communicate([TDWUtils.create_empty_room(12, 12),
+               {"$type": "set_screen_size",
+                           "width": 512,
+                           "height": 512},
                c.get_add_object(model_name="rh10",
-                                object_id=Controller.get_unique_id(),
+                                object_id=obj_id,
                                 position={"x": 0, "y": 0, "z": 0.5})])
+c.communicate([])
+c.communicate({"$type": "set_leap_motion_graspable",
+               "id": obj_id})
 while True:
     c.communicate([])

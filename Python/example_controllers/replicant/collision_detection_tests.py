b5c07247e8fd592bd0a88b25771fd30c1354104a
Seth Alter <alters@mit.edu>
2022-10-13 15:44:51 -0400

    tests

--- Python/example_controllers/replicant/collision_detection_tests.py
+++ Python/example_controllers/replicant/collision_detection_tests.py
@@ -1,27 +1,39 @@
 from tdw.controller import Controller
 from tdw.tdw_utils import TDWUtils
 from tdw.add_ons.replicant import Replicant
+from tdw.add_ons.third_person_camera import ThirdPersonCamera
 from tdw.replicant.action_status import ActionStatus
 
 
 class CollisionDetectionTests(Controller):
+    """
+    Test Replicant collision detection.
+    """
+
     OBJECT_ID: int = 1
 
     def test(self, status: ActionStatus, kinematic: bool = False, walls: bool = True, avoid: bool = True,
              exclude: bool = False, distance: float = 4) -> None:
         """
-        Initialize the scene.
+        Test collision detection.
 
-        :param kinematic: If True, the trunk is kinematic.
+        :param status: The expected action status at the end of the move_by(distance) action.
+        :param kinematic: If True, the object is kinematic.
+        :param walls: If True, listen to collisions with walls.
+        :param avoid: If True, try to avoid obstacles and walls.
+        :param exclude: If True, ignore collisions with the object.
+        :param distance: The distance to move by.
         """
 
         self.add_ons.clear()
-        r = Replicant()
-        self.add_ons.append(r)
-        r.collision_detection.walls = walls
-        r.collision_detection.avoid = avoid
+        replicant = Replicant()
+        camera = ThirdPersonCamera(position={"x": 2, "y": 3, "z": 0.3},
+                                   look_at=replicant.replicant_id)
+        self.add_ons.extend([replicant, camera])
+        replicant.collision_detection.walls = walls
+        replicant.collision_detection.avoid = avoid
         if exclude:
-            r.collision_detection.exclude_objects.append(CollisionDetectionTests.OBJECT_ID)
+            replicant.collision_detection.exclude_objects.append(CollisionDetectionTests.OBJECT_ID)
         commands = [{"$type": "load_scene",
                      "scene_name": "ProcGenScene"},
                     TDWUtils.create_empty_room(12, 12)]
@@ -30,13 +42,17 @@ class CollisionDetectionTests(Controller):
                                                           position={"x": 0, "y": 0, "z": 3},
                                                           kinematic=kinematic))
         self.communicate(commands)
-        r.move_by(distance)
-        while r.action.status == ActionStatus.ongoing:
+        replicant.move_by(distance)
+        while replicant.action.status == ActionStatus.ongoing:
             self.communicate([])
-        assert r.action.status == status, r.action.status
+        assert replicant.action.status == status, replicant.action.status
 
 
 if __name__ == "__main__":
     c = CollisionDetectionTests()
-    c.test(kinematic=False, walls=True, status=ActionStatus.detected_obstacle)
+    c.test(status=ActionStatus.detected_obstacle)
+    c.test(avoid=False, status=ActionStatus.collision)
+    c.test(avoid=False, exclude=True, status=ActionStatus.success)
+    c.test(distance=-12, status=ActionStatus.detected_obstacle)
+    c.test(kinematic=True, status=ActionStatus.detected_obstacle)
     c.communicate({"$type": "terminate"})

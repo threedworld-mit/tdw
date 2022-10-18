cf65a1cd65594084beda3bbda212b9ae5565fc63
Seth Alter <alters@mit.edu>
2022-10-18 11:33:16 -0400

    Cleaned up Animate

--- Python/tdw/replicant/actions/animate.py
+++ Python/tdw/replicant/actions/animate.py
@@ -13,6 +13,8 @@ from tdw.librarian import HumanoidAnimationLibrarian, HumanoidAnimationRecord
 class Animate(Action):
     """
     Play an animation.
+
+    The animation will end either when the animation clip is finished or if the Replicant collides with something (see `self.collision_detection`).
     """
 
     def __init__(self, animation: str, collision_detection: CollisionDetection, forward: bool = True,
@@ -29,14 +31,22 @@ class Animate(Action):
         # Add the animation library.
         if library not in Controller.HUMANOID_ANIMATION_LIBRARIANS:
             Controller.HUMANOID_ANIMATION_LIBRARIANS[library] = HumanoidAnimationLibrarian(library)
-        # Get the animation record.
-        self._record: HumanoidAnimationRecord = Controller.HUMANOID_ANIMATION_LIBRARIANS[library].get_record(animation)
-        self._collision_detection: CollisionDetection = collision_detection
+        """:field
+        The `HumanoidAnimationRecord` of the animation.
+        """
+        self.record: HumanoidAnimationRecord = Controller.HUMANOID_ANIMATION_LIBRARIANS[library].get_record(animation)
+        """:field
+        The [`CollisionDetection`](../collision_detection.md) rules.
+        """
+        self.collision_detection: CollisionDetection = collision_detection
         # Don't try to play the same animation twice if the first one ended in a collision.
-        if self._collision_detection.previous_was_same and previous is not None and isinstance(previous, Animate) and \
-                previous.status == ActionStatus.collision and previous._record.name == self._record.name:
+        if self.collision_detection.previous_was_same and previous is not None and isinstance(previous, Animate) and \
+                previous.status == ActionStatus.collision and previous.record.name == self.record.name:
             self.status = ActionStatus.collision
-        self._forward: bool = forward
+        """:field
+        If True, play the animation forwards. If False, play the animation backwards.
+        """
+        self.forward: bool = forward
 
     def get_initialization_commands(self, resp: List[bytes], static: ReplicantStatic, dynamic: ReplicantDynamic,
                                     image_frequency: ImageFrequency) -> List[dict]:
@@ -44,18 +54,18 @@ class Animate(Action):
                                                        image_frequency=image_frequency)
         # Download the animation if needed. Play the animation.
         commands.extend([{"$type": "add_humanoid_animation",
-                          "name": self._record.name,
-                          "url": self._record.get_url()},
+                          "name": self.record.name,
+                          "url": self.record.get_url()},
                          {"$type": "play_humanoid_animation",
-                          "name": self._record.name,
+                          "name": self.record.name,
                           "id": static.replicant_id,
-                          "framerate": self._record.framerate,
-                          "forward": self._forward}])
+                          "framerate": self.record.framerate,
+                          "forward": self.forward}])
         return commands
 
     def get_ongoing_commands(self, resp: List[bytes], static: ReplicantStatic, dynamic: ReplicantDynamic) -> List[dict]:
         # If there was a collision, stop the animation.
-        if len(dynamic.get_collision_enters(collision_detection=self._collision_detection)) > 0:
+        if len(dynamic.get_collision_enters(collision_detection=self.collision_detection)) > 0:
             self.status = ActionStatus.collision
         # Continue the animation.
         elif self._get_motion_complete(replicant_id=static.replicant_id, resp=resp):

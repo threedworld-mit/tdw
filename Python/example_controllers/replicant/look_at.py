a360d6c71fb0ca949d9e1cfa6672bdb429353715
Seth Alter <alters@mit.edu>
2022-10-18 11:04:57 -0400

    look-at works

--- Python/example_controllers/replicant/look_at.py
+++ Python/example_controllers/replicant/look_at.py
@@ -1,10 +1,8 @@
 from tdw.controller import Controller
 from tdw.tdw_utils import TDWUtils
 from tdw.add_ons.replicant import Replicant
-from tdw.add_ons.third_person_camera import ThirdPersonCamera
 from tdw.add_ons.image_capture import ImageCapture
 from tdw.replicant.action_status import ActionStatus
-from tdw.agents.arm import Arm
 from tdw.agents.image_frequency import ImageFrequency
 from tdw.backend.paths import EXAMPLE_CONTROLLER_OUTPUT_PATH
 
@@ -17,10 +15,7 @@ def do_action(status: ActionStatus = ActionStatus.success):
 
 c = Controller()
 replicant = Replicant(image_frequency=ImageFrequency.always)
-camera = ThirdPersonCamera(position={"x": 2, "y": 3, "z": 0.3},
-                           look_at=replicant.replicant_id,
-                           avatar_id="a")
-c.add_ons.extend([replicant, camera])
+c.add_ons.append(replicant)
 object_id = Controller.get_unique_id()
 # Create the room. Set a target target.
 commands = [TDWUtils.create_empty_room(12, 12),
@@ -32,21 +27,11 @@ commands.extend(Controller.get_add_physics_object(model_name="basket_18inx18inx1
 c.communicate(commands)
 path = EXAMPLE_CONTROLLER_OUTPUT_PATH.joinpath("replicant_look_at")
 print(f"Images will be saved to: {path}")
-capture = ImageCapture(avatar_ids=[camera.avatar_id, replicant.static.avatar_id], path=path)
+capture = ImageCapture(avatar_ids=[replicant.static.avatar_id], path=path)
 c.add_ons.append(capture)
 c.communicate([])
 replicant.look_at(target=object_id)
 do_action()
-exit()
-do_action(status=ActionStatus.detected_obstacle)
-replicant.reach_for(target=object_id, arms=Arm.right)
-do_action()
-replicant.grasp(target=object_id, arm=Arm.right)
-do_action()
-replicant.move_by(-2)
-do_action()
-replicant.drop(arm=Arm.right)
-do_action()
-replicant.reset_arm(arms=Arm.right)
+replicant.look_at(target={"x": 2, "y": 0.3, "z": 3})
 do_action()
 c.communicate({"$type": "terminate"})

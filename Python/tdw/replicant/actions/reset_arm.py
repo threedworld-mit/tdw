67979ad27c37619957b57a9b919eaebd5d202668
Seth Alter <alters@mit.edu>
2022-10-18 12:17:22 -0400

    Cleaned up ResetArm

--- Python/tdw/replicant/actions/reset_arm.py
+++ Python/tdw/replicant/actions/reset_arm.py
@@ -8,6 +8,11 @@ from tdw.replicant.actions.arm_motion import ArmMotion
 class ResetArm(ArmMotion):
     """
     Move arm(s) back to rest position.
+
+    The Replicant's arm(s) will continuously over multiple `communicate()` calls move until either the motion is complete or the arm collides with something (see `self.collision_detection`).
+
+    - The collision detection will respond normally to walls, objects, obstacle avoidance, etc.
+    - If `self.collision_detection.previous_was_same == True`, and if the previous action was a subclass of `ArmMotion`, and it ended in a collision, this action ends immediately.
     """
 
     def get_initialization_commands(self, resp: List[bytes], static: ReplicantStatic, dynamic: ReplicantDynamic,
@@ -16,6 +21,6 @@ class ResetArm(ArmMotion):
                                                        image_frequency=image_frequency)
         commands.extend([{"$type": "replicant_reset_arm",
                           "id": static.replicant_id,
-                          "duration": self._duration,
-                          "arm": arm.name} for arm in self._arms])
+                          "duration": self.duration,
+                          "arm": arm.name} for arm in self.arms])
         return commands

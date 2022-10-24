f43833e7603aaf193735bf4021476cacf0e7a195
Seth Alter <alters@mit.edu>
2022-10-24 10:09:31 -0400

    rigidbody

--- Python/tdw/add_ons/replicant.py
+++ Python/tdw/add_ons/replicant.py
@@ -112,6 +112,8 @@ class Replicant(AddOn):
                      "rotation": self.initial_rotation,
                      "url": self._record.get_url(),
                      "id": self.replicant_id},
+                    {"$type": "add_replicant_rigidbody",
+                     "id": self.replicant_id},
                     {"$type": "send_replicants",
                      "frequency": "always"},
                     {"$type": "send_transforms",

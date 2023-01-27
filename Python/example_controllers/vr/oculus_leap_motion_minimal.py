a863e4f6a3be828f300587ada88128dc07e1e5fb
Seth Alter <alters@mit.edu>
2023-01-27 10:56:56 -0500

    set_teleportation_area

--- Python/example_controllers/vr/oculus_leap_motion_minimal.py
+++ Python/example_controllers/vr/oculus_leap_motion_minimal.py
@@ -6,13 +6,17 @@ from tdw.add_ons.oculus_leap_motion import OculusLeapMotion
 Minimal Oculus Leap Motion example.
 """
 
-c = Controller(launch_build=False)
+c = Controller()
 vr = OculusLeapMotion()
 c.add_ons.append(vr)
-c.communicate([TDWUtils.create_empty_room(12, 12),
-               c.get_add_object(model_name="rh10",
-                                object_id=Controller.get_unique_id(),
-                                position={"x": 0, "y": 0, "z": 0.5})])
-c.communicate({"$type": "set_teleportation_area"})
+commands = [TDWUtils.create_empty_room(12, 12)]
+commands.extend(Controller.get_add_physics_object(model_name="small_table_green_marble",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0, "y": 0, "z": 0.5},
+                                                  kinematic=True,))
+commands.extend(Controller.get_add_physics_object(model_name="vase_02",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0.2, "y": 1.0, "z": 0.5}))
+c.communicate(commands)
 while True:
     c.communicate([])

332103e974aa5e6c92267d3aefefebb8ca8ad662
Seth Alter <alters@mit.edu>
2023-01-27 12:41:45 -0500

    implausible_audio.py

--- Python/example_controllers/audio/implausible_audio.py
+++ Python/example_controllers/audio/implausible_audio.py
@@ -4,13 +4,13 @@ from tdw.tdw_utils import TDWUtils
 from tdw.add_ons.third_person_camera import ThirdPersonCamera
 from tdw.add_ons.audio_initializer import AudioInitializer
 from tdw.add_ons.physics_audio_recorder import PhysicsAudioRecorder
-from tdw.add_ons.py_impact import PyImpact
-from tdw.physics_audio.audio_material import AudioMaterial
-from tdw.physics_audio.object_audio_static import ObjectAudioStatic
+from tdw.add_ons.clatter import Clatter
+from tdw.physics_audio.impact_material import ImpactMaterial
+from tdw.physics_audio.clatter_object import ClatterObject
 from tdw.backend.paths import EXAMPLE_CONTROLLER_OUTPUT_PATH
 
 """
-Generate audio using audio physics parameters that don't match the object's actual physics parameters.
+Generate audio using physics parameters that don't match the object's actual physics parameters.
 """
 
 rng = np.random.RandomState(0)
@@ -18,7 +18,7 @@ c = Controller()
 floor_visual_material = "parquet_long_horizontal_clean"
 commands = [TDWUtils.create_empty_room(12, 12),
             c.get_add_material(material_name=floor_visual_material),
-            {"$type": "set_proc_gen_floor_material",
+            {"$type": "set_floor_material",
              "name": floor_visual_material}]
 object_id = c.get_unique_id()
 model_name = "vase_02"
@@ -26,14 +26,11 @@ commands.extend(c.get_add_physics_object(model_name=model_name,
                                          object_id=object_id,
                                          position={"x": 0, "y": 2, "z": 0}))
 # Set random static audio data values.
-object_audio = ObjectAudioStatic(name=model_name,
-                                 object_id=object_id,
-                                 mass=float(rng.uniform(4, 40)),
-                                 material=rng.choice([a for a in AudioMaterial]),
-                                 bounciness=float(rng.uniform(0, 1)),
-                                 amp=float(rng.uniform(0.1, 1)),
-                                 resonance=float(rng.uniform(0.1, 1)),
-                                 size=int(rng.randint(1, 6)))
+object_audio = ClatterObject(fake_mass=float(rng.uniform(4, 40)),
+                             impact_material=rng.choice([a for a in ImpactMaterial]),
+                             amp=float(rng.uniform(0.1, 1)),
+                             resonance=float(rng.uniform(0.1, 1)),
+                             size=int(rng.randint(1, 6)))
 # Add a listener.
 camera = ThirdPersonCamera(avatar_id="a",
                            position={"x": 1, "y": 1.6, "z": -2},
@@ -41,12 +38,13 @@ camera = ThirdPersonCamera(avatar_id="a",
 # Initialize audio.
 audio = AudioInitializer(avatar_id="a")
 # Set a non-wood floor material.
-floor_material = AudioMaterial.metal
-# Initialize PyImpact.
-py_impact = PyImpact(initial_amp=0.9, static_audio_data_overrides={object_id: object_audio}, floor=floor_material)
+floor_material = ImpactMaterial.metal
+# Initialize Clatter.
+clatter = Clatter(simulation_amp=0.9, objects={object_id: object_audio},
+                  environment=ClatterObject(floor_material, 4, 0.5, 0.1, 100))
 # Add a recorder.
 recorder = PhysicsAudioRecorder()
-c.add_ons.extend([camera, audio, py_impact, recorder])
+c.add_ons.extend([camera, audio, clatter, recorder])
 # Create the scene.
 c.communicate(commands)
 # Start recording.

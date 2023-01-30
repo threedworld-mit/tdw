129980dcbb2ddf45165f8ad76dcb03971e96dfb9
Seth Alter <alters@mit.edu>
2023-01-30 16:37:07 -0500

    thumbs up

--- Python/tdw/add_ons/oculus_leap_motion.py
+++ Python/tdw/add_ons/oculus_leap_motion.py
@@ -1,14 +1,16 @@
-from typing import List, Dict, Optional
+from os import urandom
+from typing import List, Dict, Optional, Callable
 from pathlib import Path
 from pkg_resources import resource_filename
 import numpy as np
+from tdw.tdw_utils import TDWUtils
 from tdw.add_ons.vr import VR
 from tdw.vr_data.rig_type import RigType
 from tdw.vr_data.finger_bone import FingerBone
 from tdw.vr_data.finger import Finger
 from tdw.replicant.arm import Arm
 from tdw.object_data.transform import Transform
-from tdw.output_data import OutputData, StaticRigidbodies, LeapMotion
+from tdw.output_data import OutputData, StaticRigidbodies, LeapMotion, Raycast, Overlap
 
 
 class OculusLeapMotion(VR):
@@ -26,6 +28,8 @@ class OculusLeapMotion(VR):
     The finger bones as [`FingerBone`](../vr_data/finger_bone.md) values in the order that they'll appear in this add-on's dictionaries.
     """
     BONES: List[FingerBone] = [__b for __b in FingerBone]
+    _TELEPORT_RAY_INVALID_COLOR: Dict[str, float] = {"r": 0.8, "g": 0, "b": 0, "a": 1}
+    _TELEPORT_RAY_VALID_COLOR: Dict[str, float] = {"r": 0, "g": 0, "b": 0.8, "a": 1}
 
     def __init__(self, set_graspable: bool = True, output_data: bool = True,
                  position: Dict[str, float] = None, rotation: float = 0, attach_avatar: bool = False,
@@ -83,23 +87,31 @@ class OculusLeapMotion(VR):
         """:field
         A dictionary of predefined poses. Key = An [`Arm`](../replicant/arm.md) value for the hand. Value = A dictionary. Key = The name of the pose as a string. Value = A numpy array of the pose.
         """
-        self.poses: Dict[Arm, Dict[str, np.ndarray]] = dict()
+        self.poses_definitions: Dict[Arm, Dict[str, np.ndarray]] = dict()
         # Load the poses.
         pose_directory = Path(resource_filename(__name__, "")).joinpath("../vr_data/leap_motion_poses").resolve()
         for arm in [Arm.left, Arm.right]:
-            self.poses[arm] = dict()
+            self.poses_definitions[arm] = dict()
             pose_hand_directory = pose_directory.joinpath(arm.name)
             if not pose_hand_directory.exists():
                 continue
             for f in pose_hand_directory.iterdir():
                 if f.is_file() and f.suffix == ".npy":
-                    self.poses[arm][f.stem] = np.load(f)
-        """:field
-        A dictionary of ongoing poses. Key = An [`Arm`](../replicant/arm.md) value for the hand. Value = The name of the pose (can be None).
-        """
-        self.ongoing_poses: Dict[Arm, Optional[str]] = {Arm.left: None, Arm.right: None}
+                    self.poses_definitions[arm][f.stem] = np.load(f)
+        # A dictionary of ongoing poses. Key = An Arm value for the hand. Value = The name of the pose (can be None).
+        self._poses: Dict[Arm, Optional[str]] = {Arm.left: None, Arm.right: None}
+        # A dictionary of callbacks for when a pose starts.
+        self._pose_enter_callbacks: Dict[Arm, Dict[str, Callable[[Arm, List[bytes]], None]]] = {Arm.left: dict(), Arm.right: dict()}
+        # A dictionary of callbacks for when a pose continues.
+        self._pose_stay_callbacks: Dict[Arm, Dict[str, Callable[[Arm, List[bytes]], None]]] = {Arm.left: dict(), Arm.right: dict()}
+        # A dictionary of callbacks for when a pose ends.
+        self._pose_exit_callbacks: Dict[Arm, Dict[str, Callable[[Arm, List[bytes]], None]]] = {Arm.left: dict(), Arm.right: dict()}
         # Local rotations are used for hand poses.
         self._finger_bone_angles: np.ndarray = np.zeros(shape=1)
+        self._teleport_raycast_id: int = int.from_bytes(urandom(3), byteorder='big')
+        self._can_teleport: bool = False
+        self._teleport_point: np.ndarray = np.zeros(shape=3)
+        self._has_teleport_line: bool = False
 
     def get_initialization_commands(self) -> List[dict]:
         commands = super().get_initialization_commands()
@@ -110,15 +122,6 @@ class OculusLeapMotion(VR):
             commands.append({"$type": "send_leap_motion",
                              "frequency": "always"})
         commands.append({"$type": "set_teleportation_area"})
-        # TODO remove this.
-        commands.extend([{"$type": "add_ui_canvas"},
-                         {"$type": "attach_ui_canvas_to_vr_rig"},
-                         {"$type": "add_ui_text",
-                          "text": "",
-                          "id": 0,
-                          "font_size": 22,
-                          "color": {"r": 1, "g": 0, "b": 0, "a": 1},
-                          "raycast_target": False}])
         return commands
 
     def on_send(self, resp: List[bytes]) -> None:
@@ -160,32 +163,65 @@ class OculusLeapMotion(VR):
         # Check poses.
         if self._finger_bone_angles.shape[0] == 2:
             poses: Dict[Arm, str] = dict()
-            for arm in self.poses:
-                for pose in self.poses[arm]:
-                    d = np.linalg.norm(self._finger_bone_angles[arm.value] - self.poses[arm][pose])
+            for arm in self.poses_definitions:
+                for pose in self.poses_definitions[arm]:
+                    d = np.linalg.norm(self._finger_bone_angles[arm.value] - self.poses_definitions[arm][pose])
                     if d < 35:
                         poses[arm] = pose
                         break
-            # Check for poses that have ended.
-            for arm in self.ongoing_poses:
-                if self.ongoing_poses[arm] is None and arm in poses:
-                    self.ongoing_poses[arm] = poses[arm]
-                    self.commands.append({"$type": "set_ui_text",
-                                          "id": 0,
-                                          "text": poses[arm] + " started"})
-                if self.ongoing_poses[arm] is not None and arm not in poses:
-                    self.commands.append({"$type": "set_ui_text",
-                                          "id": 0,
-                                          "text": self.ongoing_poses[arm][:] + " ended"})
-                    self.ongoing_poses[arm] = None
+            for arm in self._poses:
+                if arm in poses:
+                    # Start a new pose.
+                    if self._poses[arm] is None:
+                        self._poses[arm] = poses[arm]
+                        if self._poses[arm] in self._pose_enter_callbacks[arm]:
+                            # Callback on enter.
+                            self._pose_enter_callbacks[arm][self._poses[arm]](arm, resp)
+                    # Continue an ongoing pose.
+                    else:
+                        if self._poses[arm] in self._pose_stay_callbacks[arm]:
+                            # Callback on stay.
+                            self._pose_stay_callbacks[arm][self._poses[arm]](arm, resp)
+                else:
+                    # End a pose.
+                    if self._poses[arm] is not None:
+                        self._poses[arm] = None
+                        # Callback on exit.
+                        if self._poses[arm] in self._pose_exit_callbacks[arm]:
+                            self._pose_exit_callbacks[arm][self._poses[arm]](arm, resp)
 
-    def reset(self, non_graspable: List[int] = None, position: Dict[str, float] = None, rotation: float = 0) -> None:
+    def set_pose_callbacks(self, pose: str, hand: Arm, on_enter: Callable[[Arm, List[bytes]], None] = None,
+                           on_stay: Callable[[Arm, List[bytes]], None] = None, on_exit: Callable[[Arm, List[bytes]], None] = None) -> None:
+        """
+        Register callback methods for a pose. When the pose starts, continues, and/or exits, these methods will be invoked.
+
+        Each of the three callbacks must have two arguments: `Arm`, i.e. the hand doing the pose, and `List[Bytes]`, i.e. the response from the build, and must return None.
+
+        Optionally, each of the three callbacks can be None.
+
+        :param pose: The name of the pose. See: `self.pose_definitions`.
+        :param hand: The hand as an [`Arm`](../replicant/arm.md) value.
+        :param on_enter: A callback method to invoke when the hand enters the pose.
+        :param on_stay: A callback method to invoke when the hand stays in the pose.
+        :param on_exit: A callback method to invoke when the hand exits the pose.
+        """
+
+        if on_enter is not None:
+            self._pose_enter_callbacks[hand][pose] = on_enter
+        if on_stay is not None:
+            self._pose_stay_callbacks[hand][pose] = on_stay
+        if on_exit is not None:
+            self._pose_exit_callbacks[hand][pose] = on_exit
+
+    def reset(self, non_graspable: List[int] = None, position: Dict[str, float] = None, rotation: float = 0,
+              reset_pose_callbacks: bool = False) -> None:
         """
         Reset the VR rig. Call this whenever a scene is reset.
 
         :param non_graspable: A list of IDs of non-graspable objects. By default, all non-kinematic objects are graspable and all kinematic objects are non-graspable. Set this to make non-kinematic objects non-graspable.
         :param position: The initial position of the VR rig. If None, defaults to `{"x": 0, "y": 0, "z": 0}`
         :param rotation: The initial rotation of the VR rig in degrees.
+        :param reset_pose_callbacks: If True, reset the pose callbacks. You'll need to recreate them by calling set_pose_callbacks().
         """
 
         self._set_graspable = True
@@ -193,6 +229,13 @@ class OculusLeapMotion(VR):
             self._non_graspable = list()
         else:
             self._non_graspable = non_graspable
+        self._poses.clear()
+        self._has_teleport_line = False
+        self._can_teleport = False
+        if reset_pose_callbacks:
+            for arm in [Arm.left, Arm.right]:
+                for d in [self._pose_enter_callbacks, self._pose_stay_callbacks, self._pose_exit_callbacks]:
+                    d[arm].clear()
         super().reset(position=position, rotation=rotation)
 
     @staticmethod
@@ -246,3 +289,62 @@ class OculusLeapMotion(VR):
         for i in range(max_num_collisions):
             if leap_motion.get_is_collision(hand_index, 15, i):
                 palm_collisions.append(leap_motion.get_collision_id(hand_index, 15, i))
+
+    def _cast_teleport_ray(self, hand: Arm, resp: List[bytes]) -> None:
+        # TODO raycast and overlap
+        raycast_hit: bool = False
+        raycast_point: np.ndarray = np.zeros(shape=3)
+        overlap_ids: np.ndarray = np.zeros(shape=0)
+        overlap_env: bool = False
+        overlap_walls: bool = False
+        overlap_exists: bool = False
+        # Get the raycast and overlap from the output data.
+        for i in range(len(resp) - 1):
+            r_id = OutputData.get_data_type_id(resp[i])
+            if r_id == "rayc":
+                raycast: Raycast = Raycast(resp[i])
+                if raycast.get_raycast_id() == self._teleport_raycast_id:
+                    if raycast.get_hit() and not raycast.get_hit_object():
+                        raycast_point = np.array(raycast.get_point())
+                        hit = True
+            elif r_id == "over":
+                overlap: Overlap = Overlap(resp[i])
+                if overlap.get_id() == self._teleport_raycast_id:
+                    overlap_ids = overlap.get_object_ids()
+                    overlap_env = overlap.get_env()
+                    overlap_walls = overlap.get_walls()
+        # We're casting at the floor in an empty space.
+        if raycast_hit and raycast_point[1] <= 0.05 and overlap_env and not overlap_walls and len(overlap_ids) == 0:
+            self._can_teleport = True
+            self._teleport_point = raycast_point
+            color = OculusLeapMotion._TELEPORT_RAY_VALID_COLOR
+        # We either aren't casting or we aren't casting at a good spot.
+        else:
+            self._can_teleport = False
+            color = OculusLeapMotion._TELEPORT_RAY_INVALID_COLOR
+        # Get the origin point.
+        hand_position: np.ndarray = self.left_hand.position if hand == Arm.left else self.right_hand.position
+        # The raycast exists and hit something.
+        if hit:
+            points = [TDWUtils.array_to_vector3(hand_position), TDWUtils.array_to_vector3(raycast_point)]
+            # Update an existing line.
+            if self._has_teleport_line:
+                self.commands.extend([{"$type": "set_line_renderer_points",
+                                       "id": self._teleport_raycast_id,
+                                       "points": points},
+                                      {"$type": "set_line_renderer_color",
+                                       "start_color": color,
+                                       "end_color": color,
+                                       "points": points,
+                                       "id": self._teleport_raycast_id}])
+            # Create a new line.
+            else:
+                self._has_teleport_line = True
+                self.commands.append({"$type": "add_line_renderer",
+                                      "points": points,
+                                      "start_color": color,
+                                      "end_color": color,
+                                      "id": self._teleport_raycast_id})
+        # Cast a ray and an overlap.
+
+

e9ddb11af3b2a2b9c071bac5ace9ebbc6ca7026a
Seth Alter <alters@mit.edu>
2023-02-14 09:12:18 -0500

    Fixed focal length regex

--- Python/tdw/vray_data/vray_camera.py
+++ Python/tdw/vray_data/vray_camera.py
@@ -25,16 +25,20 @@ class VRayCamera:
         The line number of the focal length line.
         """
         self.focal_length_line_number: int = -1
+        got_focal_length = False
+        got_render_view = False
         lines = text.split("\n")
         render_view_pattern = re.compile("RenderView")
         camera_physical = re.compile("CameraPhysical")
         for i, line in enumerate(lines):
-            if render_view_pattern.search(line):
+            if not got_render_view and render_view_pattern.search(line):
                 # We will want to replace the line following the "RenderView" line, with the transform data.
                 self.render_view_line_number = i
-            if camera_physical.search(line):
+                got_render_view = True
+            if not got_focal_length and camera_physical.search(line):
                 # We will want to replace the third line following the "CameraPhysical" line, with the TDW focal length.
                 self.focal_length_line_number = i + 3
+                got_focal_length = True
         if self.render_view_line_number < 0:
             raise Exception(f"RenderView not found in {path}")
         if self.focal_length_line_number < 0:

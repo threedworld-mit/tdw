c43bb57db3f705e0a0865f39171816f41e490033
Seth Alter <alters@mit.edu>
2023-03-03 15:12:50 -0500

    improved example controller

--- Python/example_controllers/vr/oculus_leap_motion_minimal.py
+++ Python/example_controllers/vr/oculus_leap_motion_minimal.py
@@ -6,13 +6,25 @@ from tdw.add_ons.oculus_leap_motion import OculusLeapMotion
 Minimal Oculus Leap Motion example.
 """
 
-c = Controller(launch_build=False)
-vr = OculusLeapMotion()
+c = Controller()
+commands = [TDWUtils.create_empty_room(12, 12)]
+z = 0.8
+table_id = Controller.get_unique_id()
+commands.extend(Controller.get_add_physics_object(model_name="small_table_green_marble",
+                                                  object_id=table_id,
+                                                  position={"x": 0, "y": 0, "z": z},
+                                                  kinematic=True))
+commands.extend(Controller.get_add_physics_object(model_name="woven_box",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0, "y": 1, "z": z},
+                                                  default_physics_values=False,
+                                                  mass=100))
+# Add the ball object and make it graspable.
+commands.extend(Controller.get_add_physics_object(model_name="vase_02",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0, "y": 1.25, "z": z}))
+vr = OculusLeapMotion(non_graspable=[table_id])
 c.add_ons.append(vr)
-c.communicate([TDWUtils.create_empty_room(12, 12),
-               c.get_add_object(model_name="rh10",
-                                object_id=Controller.get_unique_id(),
-                                position={"x": 0, "y": 0, "z": 0.5})])
-c.communicate({"$type": "set_teleportation_area"})
+c.communicate(commands)
 while True:
     c.communicate([])

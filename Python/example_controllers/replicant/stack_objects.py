d19b5baf59cda1cda995e0b3faafe84163ea32fe
Seth Alter <alters@mit.edu>
2023-04-20 14:06:39 -0400

    hmm

--- Python/example_controllers/replicant/stack_objects.py
+++ Python/example_controllers/replicant/stack_objects.py
@@ -4,14 +4,13 @@ from itertools import permutations
 import numpy as np
 from tdw.add_ons.third_person_camera import ThirdPersonCamera
 from tdw.add_ons.image_capture import ImageCapture
-from tdw.add_ons.nav_mesh import NavMesh
 from tdw.add_ons.replicant import Replicant
 from tdw.add_ons.object_manager import ObjectManager
 from tdw.add_ons.empty_object_manager import EmptyObjectManager
 from tdw.replicant.arm import Arm
 from tdw.replicant.action_status import ActionStatus
 from tdw.replicant.ik_plans.ik_plan_type import IkPlanType
-from tdw.output_data import OutputData, Raycast, NavMeshPath
+from tdw.output_data import OutputData, Raycast
 from tdw.tdw_utils import TDWUtils
 from tdw.controller import Controller
 from tdw.backend.paths import EXAMPLE_CONTROLLER_OUTPUT_PATH
@@ -29,7 +28,7 @@ class ReplicantState(Enum):
 
 
 class StackObjects(Controller):
-    def __init__(self, object_scale: float = 0.2, port: int = 1071, check_version: bool = True, launch_build: bool = True):
+    def __init__(self, object_scale: float = 0.3, port: int = 1071, check_version: bool = True, launch_build: bool = True):
         super().__init__(port=port, check_version=check_version, launch_build=launch_build)
         self.replicant = Replicant()
         self.camera = ThirdPersonCamera(position={"x": 0, "y": 2.2, "z": -2.61},
@@ -38,11 +37,8 @@ class StackObjects(Controller):
         path = EXAMPLE_CONTROLLER_OUTPUT_PATH.joinpath("replicant_stack_objects")
         print(f"Images will be saved to: {path}")
         self.capture = ImageCapture(avatar_ids=["a"], path=path)
-        self.nav_mesh = NavMesh(exclude_objects=[self.replicant.replicant_id])
-        self.object_manager = ObjectManager()
+        self.object_manager = ObjectManager(transforms=True, bounds=True, rigidbodies=False)
         self.empty_object_manager = EmptyObjectManager()
-        self.nav_mesh_path: np.ndarray = np.zeros(shape=0)
-        self.nav_mesh_path_index: int = 0
         self.replicant_state: ReplicantState = ReplicantState.moving_to_cube
         self.cubes: List[int] = list()
         self.stack_position: Dict[str, float] = dict()
@@ -56,7 +52,6 @@ class StackObjects(Controller):
         self.replicant.reset()
         self.camera.initialized = False
         self.capture.initialized = False
-        self.nav_mesh.initialized = False
         self.object_manager.reset()
         # Add empty objects to the Replicant. This will be used to reset an arm holding an object.
         empty_object_positions: Dict[int, List[dict]] = {self.replicant.replicant_id: list()}
@@ -73,10 +68,9 @@ class StackObjects(Controller):
                      "scene_name": "ProcGenScene"},
                     TDWUtils.create_empty_room(12, 12)]
         # Create an occupancy map. This works because we know that there is only one room and we know its dimensions.
-        object_occupancy_map: np.ndarray = np.array(np.mgrid[-5.1:5.1, -5.1:5.1])
-        xs = object_occupancy_map[0].flatten()
-        zs = object_occupancy_map[1].flatten()
-        indices = np.array(list(permutations(np.arange(0, object_occupancy_map.shape[1], dtype=int), 2)))
+        xs = np.linspace(-5.1, 5.1, 12)
+        zs = xs.copy()
+        indices = np.array(list(permutations(np.arange(0, xs.shape[0], dtype=int), 2)))
         indices_indices = np.arange(0, indices.shape[0], dtype=int)
         # Create a random number generator.
         if random_seed is None:
@@ -124,8 +118,8 @@ class StackObjects(Controller):
         # Set a position for the stack.
         ix, iz = indices[indices_indices[num_objects]]
         self.stack_position = {"x": float(xs[ix]), "y": 0, "z": float(zs[iz])}
-        # Add the Replicant, the ObjectManager, the OccupancyMap, the NavMesh, and the EmptyObjectManager.
-        self.add_ons.extend([self.replicant, self.object_manager, self.nav_mesh, self.empty_object_manager])
+        # Add the Replicant, the ObjectManager, the OccupancyMap, and the EmptyObjectManager.
+        self.add_ons.extend([self.replicant, self.object_manager, self.empty_object_manager])
         # Create the scene.
         self.communicate(commands)
         # Add a camera and enable image capture.
@@ -142,23 +136,10 @@ class StackObjects(Controller):
         # Navigate to the next cube.
         if self.replicant_state == ReplicantState.moving_to_cube:
             if self.replicant.action.status != ActionStatus.ongoing:
-                # In an actual use-case, you should implement a way to deal with failures.
-                # In this example, for the sake of brevity we'll just print a warning.
-                if self.replicant.action.status != ActionStatus.success:
-                    print(f"Warning! Failed to move to {self.nav_mesh_path[self.nav_mesh_path_index]}")
-                # We arrived at the target. Reach for the cube.
-                if self.nav_mesh_path_index == self.nav_mesh_path.shape[0] - 1:
-                    self.replicant_state = ReplicantState.reaching_for_cube
-                    self.communicate([])
-                    self.replicant.reach_for(target=self.cubes[self.cube_index], arm=Arm.right)
-                # This is the last point in the NavMesh. Move to the cube. Apply an offset to the target position.
-                elif self.nav_mesh_path_index == self.nav_mesh_path.shape[0] - 2:
-                    self.replicant.move_to(target=self.cubes[self.cube_index], arrived_at=0.25)
-                    self.nav_mesh_path_index += 1
-                # Move to the next waypoint.
-                else:
-                    self.nav_mesh_path_index += 1
-                    self.replicant.move_to(target=self.nav_mesh_path[self.nav_mesh_path_index])
+                # Reach for the cube.
+                self.replicant_state = ReplicantState.reaching_for_cube
+                self.communicate([])
+                self.replicant.reach_for(target=self.cubes[self.cube_index], arm=Arm.right)
         # Reach for the next cube.
         elif self.replicant_state == ReplicantState.reaching_for_cube:
             if self.replicant.action.status != ActionStatus.ongoing:
@@ -174,13 +155,13 @@ class StackObjects(Controller):
             if self.replicant.action.status != ActionStatus.ongoing:
                 if self.replicant.action.status != ActionStatus.success:
                     print(f"Warning! Failed to grasp {self.cubes[self.cube_index]}")
-            # Start to reset the arm holding the cube.
-            self.replicant_state = ReplicantState.resetting_arm_with_cube
-            self.communicate([])
-            # Get the target position from the Replicant's empty objects.
-            empty_object_index = Arm.right.value
-            target = self.empty_object_manager.empty_objects[self.replicant.replicant_id][empty_object_index]
-            self.replicant.reach_for(target=target, arm=Arm.right)
+                # Start to reset the arm holding the cube.
+                self.replicant_state = ReplicantState.resetting_arm_with_cube
+                self.communicate([])
+                # Get the target position from the Replicant's empty objects.
+                empty_object_index = Arm.right.value
+                target = self.empty_object_manager.empty_objects[self.replicant.replicant_id][empty_object_index]
+                self.replicant.reach_for(target=target, arm=Arm.right)
         elif self.replicant_state == ReplicantState.resetting_arm_with_cube:
             if self.replicant.action.status != ActionStatus.ongoing:
                 if self.replicant.action.status != ActionStatus.success:
@@ -188,31 +169,27 @@ class StackObjects(Controller):
                 # Start to move to the stack.
                 self.replicant_state = ReplicantState.moving_to_stack
                 self.communicate([])
-                self.set_path(destination=self.stack_position)
+                self.replicant.move_to(target=self.stack_position, arrived_at=0.7, reset_arms=False)
         # Navigate to the stack.
         elif self.replicant_state == ReplicantState.moving_to_stack:
             if self.replicant.action.status != ActionStatus.ongoing:
                 if self.replicant.action.status != ActionStatus.success:
-                    print(f"Warning! Failed to move to {self.nav_mesh_path[self.nav_mesh_path_index]}")
-                # We arrived at the target.
-                if self.nav_mesh_path_index == self.nav_mesh_path.shape[0] - 1:
-                    # Get the target position.
-                    target_position = {k: v for k, v in self.stack_position.items()}
-                    dy = self.object_scale * 2
-                    target_position[1] = dy + self.raycast_stack()
-                    # Reach for the point above the stack. Offset the target by the held object. Use an IK plan.
-                    self.replicant.reach_for(target=self.cubes[self.cube_index], arm=Arm.right,
-                                             from_held=True, plan=IkPlanType.vertical_horizontal)
-                    # Set the state.
-                    self.replicant_state = ReplicantState.reaching_above_stack
-                # This is the last point in the NavMesh. Move to the stack with an offset.
-                elif self.nav_mesh_path_index == self.nav_mesh_path.shape[0] - 2:
-                    self.replicant.move_to(target=self.stack_position, arrived_at=0.25)
-                    self.nav_mesh_path_index += 1
-                # Move to the next waypoint.
-                else:
-                    self.nav_mesh_path_index += 1
-                    self.replicant.move_to(target=self.nav_mesh_path[self.nav_mesh_path_index])
+                    print(f"Warning! Failed to move to stack.")
+                self.communicate([])
+                # Get the target position above the stack.
+                target_position = {k: v for k, v in self.stack_position.items()}
+                dy = self.object_scale * 2
+                target_position["y"] = dy + self.raycast_stack()
+                # Reach for the point above the stack. Offset the target by the held object. Use an IK plan.
+                self.replicant.reach_for(target=target_position,
+                                         arm=Arm.right,
+                                         from_held=True,
+                                         held_point="center",
+                                         plan=IkPlanType.vertical_horizontal if self.cube_index > 0 else None,
+                                         duration=2,
+                                         arrived_at=0.02)
+                # Set the state.
+                self.replicant_state = ReplicantState.reaching_above_stack
         # Reach above the stack.
         elif self.replicant_state == ReplicantState.reaching_above_stack:
             if self.replicant.action.status != ActionStatus.ongoing:
@@ -232,7 +209,7 @@ class StackObjects(Controller):
                 # Start to move away.
                 self.replicant_state = ReplicantState.backing_away
                 self.communicate([])
-                self.replicant.move_by(distance=-0.5, reset_arms=False)
+                self.replicant.move_by(distance=-0.5)
         # Reset the arm.
         elif self.replicant_state == ReplicantState.backing_away:
             if self.replicant.action.status != ActionStatus.ongoing:
@@ -260,30 +237,7 @@ class StackObjects(Controller):
 
         cube_position = TDWUtils.array_to_vector3(self.object_manager.transforms[self.cubes[self.cube_index]].position)
         cube_position["y"] = 0
-        # Set a path to the cube.
-        self.set_path(destination=cube_position)
-        # Start moving.
-        self.replicant.move_to(target=self.nav_mesh_path[self.nav_mesh_path_index])
-
-    def set_path(self, destination: Dict[str, float]) -> None:
-        """
-        Set the Replicant's path.
-
-        :param destination: The destination position.
-        """
-
-        # Request a NavMeshPath.
-        resp = self.communicate({"$type": "send_nav_mesh_path",
-                                 "origin": TDWUtils.array_to_vector3(self.replicant.dynamic.transform.position),
-                                 "destination": destination,
-                                 "id": self.replicant.replicant_id})
-        # Get the path.
-        for i in range(len(resp) - 1):
-            r_id = OutputData.get_data_type_id(resp[i])
-            if r_id == "path":
-                self.nav_mesh_path = NavMeshPath(resp[i]).get_path()[1:]
-                self.nav_mesh_path_index = 0
-                break
+        self.replicant.move_to(target=self.cubes[self.cube_index], arrived_at=0.25)
 
     def raycast_stack(self) -> float:
         """

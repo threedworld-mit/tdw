f1ca0898b93cb6661097bd179264e3b252cbd6fe
Seth Alter <alters@mit.edu>
2023-04-27 14:40:57 -0400

    different primitives

--- Python/example_controllers/replicant/stack_objects.py
+++ Python/example_controllers/replicant/stack_objects.py
@@ -20,13 +20,13 @@ class ReplicantState(Enum):
     Enum values describing the current state of the Replicant.
     """
 
-    moving_to_cube = 0  # The Replicant is moving to a cube it wants to carry to the stack.
-    reaching_for_cube = 1  # The Replicant is reaching for a cube it wants to carry to the stack.
-    grasping_cube = 2  # The Replicant is grasping for a cube it wants to carry to the stack.
-    resetting_arm_with_cube = 3  # The Replicant is holding a cube and is resetting its arm to a neutral holding position.
-    moving_to_stack = 4  # The Replicant is holding a cube and is carrying it towards the stack.
-    reaching_above_stack = 5  # The Replicant is positioning a held cube above the stack.
-    dropping_cube = 6  # The Replicant has dropped the cube. The cube is falling onto the stack.
+    moving_to_object = 0  # The Replicant is moving to a object it wants to carry to the stack.
+    reaching_for_object = 1  # The Replicant is reaching for a object it wants to carry to the stack.
+    grasping_object = 2  # The Replicant is grasping for a object it wants to carry to the stack.
+    resetting_arm_with_object = 3  # The Replicant is holding a object and is resetting its arm to a neutral holding position.
+    moving_to_stack = 4  # The Replicant is holding a object and is carrying it towards the stack.
+    reaching_above_stack = 5  # The Replicant is positioning a held object above the stack.
+    dropping_object = 6  # The Replicant has dropped the object. The object is falling onto the stack.
     backing_away = 7  # The Replicant is backing away from the stack.
 
 
@@ -39,11 +39,16 @@ class StackObjects(Controller):
     For more information, read: `Documentation/lessons/replicants/arm_articulation_4.md`
     """
 
+    # A list of all possible models.
+    MODELS: List[str] = ["cube", "cylinder", "pentagon"]
+    # Scale the height of the model by this factor (after applying the overall object scale).
+    MODEL_HEIGHT_SCALES: List[float] = [1, 0.5, 0.5]
+
     def __init__(self, object_scale: float = 0.3, port: int = 1071, check_version: bool = True, launch_build: bool = True):
         """
         This is a standard `Controller` constructor with an additional field: `object_scale`.
 
-        `object_scale` sets the scale factor of the cubes.
+        `object_scale` sets the scale factor of the objects.
         """
 
         super().__init__(port=port, check_version=check_version, launch_build=launch_build)
@@ -61,41 +66,41 @@ class StackObjects(Controller):
         self.object_manager = ObjectManager(transforms=True, bounds=True, rigidbodies=False)
 
         # This tracks the current state of the Replicant.
-        self.replicant_state: ReplicantState = ReplicantState.moving_to_cube
+        self.replicant_state: ReplicantState = ReplicantState.moving_to_object
 
-        # A list of IDs of each cube object in the scene.
-        self.cubes: List[int] = list()
-        # The index in `self.cubes` of the cube the Replicant is trying to put on the stack.
-        self.cube_index: int = 0
+        # A list of IDs of each object object in the scene.
+        self.objects: List[int] = list()
+        # The index in `self.objects` of the object the Replicant is trying to put on the stack.
+        self.object_index: int = 0
 
         # The position of the stack.
         # For the simplicity's sake, this is always (0, 0, 0) but you could put it somewhere else, randomize it, etc.
         self.stack_position: Dict[str, float] = {"x": 0, "y": 0, "z": 0}
 
-        # The current height of the stack of cubes.
-        # This is used to determine whether the Replicant successfully placed a cube on the stack.
+        # The current height of the stack of objects.
+        # This is used to determine whether the Replicant successfully placed a object on the stack.
         self.stack_y: float = 0
 
-        # Each cube will be scaled by this factor.
-        self.object_scale_factor: Dict[str, float] = {"x": object_scale, "y": object_scale, "z": object_scale}
+        # Each object will be scaled by this factor.
+        self.object_scale: float = object_scale
 
-        # When the Replicant is holding a cube and walking, its right hand will be at this position relative to its body.
+        # When the Replicant is holding a object and walking, its right hand will be at this position relative to its body.
         self.hold_object_position = {"x": 0.2, "y": 1, "z": 0.7}
-        # When the Replicant is holding a cube, the cube will be offset from the hand by this distance.
+        # When the Replicant is holding a object, the object will be offset from the hand by this distance.
         self.offset: float = object_scale / 3
 
     def run(self, random_seed: int = None, num_objects: int = 3) -> None:
         """
         Run a trial.
 
-        A trial starts with a Replicant in the center of the room and cubes scattered around it.
+        A trial starts with a Replicant in the center of the room and objects scattered around it.
 
-        A trial ends when either the Replicant stacks all the cubes.
+        A trial ends when either the Replicant stacks all the objects.
 
         If there is an error during the trial, it will hang indefinitely.
 
         :param random_seed: The random seed for the trial. If None, the seed is random.
-        :param num_objects: The number of cubes in the scene.
+        :param num_objects: The number of objects in the scene.
         """
 
         # Reset the add-ons.
@@ -105,10 +110,10 @@ class StackObjects(Controller):
         self.capture.initialized = False
         self.object_manager.reset()
         # Reset the state and the stack.
-        self.replicant_state = ReplicantState.moving_to_cube
-        self.cubes.clear()
+        self.replicant_state = ReplicantState.moving_to_object
+        self.objects.clear()
         self.stack_y = 0
-        self.cube_index = 0
+        self.object_index = 0
         # Initialize the scene.
         commands = [{"$type": "load_scene",
                      "scene_name": "ProcGenScene"},
@@ -118,32 +123,39 @@ class StackObjects(Controller):
             rng = np.random.RandomState()
         else:
             rng = np.random.RandomState(random_seed)
-        # Get random colors for the cubes.
+        # Get random colors for the objects.
         color_arrs = rng.uniform(0, 1, num_objects * 3).reshape(num_objects, 3)
         colors = []
         for color in color_arrs:
             colors.append({"r": float(color[0]), "g": float(color[1]), "b": float(color[2]), "a": 1})
-        # Get random rotations for the cubes.
+        # Get random rotations for the objects.
         object_rotations = rng.uniform(-90, 90, num_objects)
-        # Add cubes in a circle around the Replicant.
+        # Get random models.
+        model_indices = np.arange(0, len(StackObjects.MODELS))
+        rng.shuffle(model_indices)
+        # Add objects in a circle around the Replicant.
         angle = 2 * pi / num_objects
         for i in range(num_objects):
             # Get the distance of the object from the center of the room.
-            cube_r = float(rng.uniform(1.5, 4.8))
+            object_r = float(rng.uniform(1.5, 4.8))
             # Get the position of the object by multiplying the angle by the radius.
-            position = {"x": cube_r * cos(angle * i), "y": 0, "z": cube_r * sin(angle * i)}
+            position = {"x": object_r * cos(angle * i), "y": 0, "z": object_r * sin(angle * i)}
             # Get an object ID.
             object_id = Controller.get_unique_id()
+            # Get the mode name.
+            model_name = StackObjects.MODELS[i]
+            # Get the scale.
+            scale = {"x": self.object_scale, "y": self.object_scale * StackObjects.MODEL_HEIGHT_SCALES[i], "z": self.object_scale}
             # Add the object.
-            # Set high friction and low bounciness to make it easier for the cube to stay on the stack.
-            commands.extend(Controller.get_add_physics_object(model_name="cube",
+            # Set high friction and low bounciness to make it easier for the object to stay on the stack.
+            commands.extend(Controller.get_add_physics_object(model_name=model_name,
                                                               object_id=object_id,
                                                               library="models_flex.json",
                                                               position=position,
                                                               rotation={"x": 0, "y": object_rotations[i], "z": 0},
                                                               default_physics_values=False,
                                                               mass=1,
-                                                              scale_factor=self.object_scale_factor,
+                                                              scale_factor=scale,
                                                               dynamic_friction=0.95,
                                                               static_friction=0.95,
                                                               bounciness=0.01,
@@ -152,9 +164,9 @@ class StackObjects(Controller):
             commands.append({"$type": "set_color",
                              "id": object_id,
                              "color": colors[i]})
-            # Remember the ID of the cube.
-            self.cubes.append(object_id)
-        # Move cubes to this position.
+            # Remember the ID of the object.
+            self.objects.append(object_id)
+        # Move objects to this position.
         self.stack_position = {"x": 0, "y": 0, "z": 0}
         # Add the Replicant, the ObjectManager, the OccupancyMap, and the EmptyObjectManager.
         self.add_ons.extend([self.replicant, self.object_manager])
@@ -165,50 +177,50 @@ class StackObjects(Controller):
         # Bake the NavMesh.
         self.communicate([])
         # Start moving.
-        self.start_moving_to_cube()
+        self.start_moving_to_object()
         # Build the stack.
         while not self.evaluate_replicant():
             self.communicate([])
 
     def evaluate_replicant(self) -> bool:
-        # Navigate to the next cube.
-        if self.replicant_state == ReplicantState.moving_to_cube:
+        # Navigate to the next object.
+        if self.replicant_state == ReplicantState.moving_to_object:
             if self.replicant.action.status != ActionStatus.ongoing:
-                self.replicant_state = ReplicantState.reaching_for_cube
+                self.replicant_state = ReplicantState.reaching_for_object
                 self.communicate([])
 
-                # Reach for the cube.
+                # Reach for the object.
                 # This is a simple rotation that doesn't need to use the optional parameters.
-                self.replicant.reach_for(target=self.cubes[self.cube_index], arm=Arm.right)
-        # Reach for the next cube.
-        elif self.replicant_state == ReplicantState.reaching_for_cube:
-            if self.action_ended(error_message=f"reach for {self.cubes[self.cube_index]}"):
-                self.replicant_state = ReplicantState.grasping_cube
+                self.replicant.reach_for(target=self.objects[self.object_index], arm=Arm.right)
+        # Reach for the next object.
+        elif self.replicant_state == ReplicantState.reaching_for_object:
+            if self.action_ended(error_message=f"reach for {self.objects[self.object_index]}"):
+                self.replicant_state = ReplicantState.grasping_object
                 self.communicate([])
 
-                # Start to grasp the cube.
-                # `angle` and `axis` define the cube's rotation per `communicate()` call.
+                # Start to grasp the object.
+                # `angle` and `axis` define the object's rotation per `communicate()` call.
                 # `relative_to_hand=True` means that `angle` and `axis` are relative to the hand.
-                # `offset` sets an offset distance from the cube to the hand.
-                self.replicant.grasp(target=self.cubes[self.cube_index],
+                # `offset` sets an offset distance from the object to the hand.
+                self.replicant.grasp(target=self.objects[self.object_index],
                                      arm=Arm.right,
                                      angle=0,
                                      axis="pitch",
                                      relative_to_hand=True,
                                      offset=self.offset)
-        # Grasp the next cube.
-        elif self.replicant_state == ReplicantState.grasping_cube:
-            if self.action_ended(error_message=f"grasp {self.cubes[self.cube_index]}"):
-                self.replicant_state = ReplicantState.resetting_arm_with_cube
+        # Grasp the next object.
+        elif self.replicant_state == ReplicantState.grasping_object:
+            if self.action_ended(error_message=f"grasp {self.objects[self.object_index]}"):
+                self.replicant_state = ReplicantState.resetting_arm_with_object
                 self.communicate([])
 
-                # Start to reset the arm holding the cube to a neutral holding position.
+                # Start to reset the arm holding the object to a neutral holding position.
                 # `absolute=False` means that the target position is relative to the Replicant's position and rotation.
                 self.replicant.reach_for(target=self.hold_object_position,
                                          arm=Arm.right,
                                          absolute=False)
-        elif self.replicant_state == ReplicantState.resetting_arm_with_cube:
-            if self.action_ended(error_message="reset arm holding cube"):
+        elif self.replicant_state == ReplicantState.resetting_arm_with_object:
+            if self.action_ended(error_message="reset arm holding object"):
                 self.replicant_state = ReplicantState.moving_to_stack
                 self.communicate([])
 
@@ -225,10 +237,10 @@ class StackObjects(Controller):
                 self.communicate([])
                 # Get a target position above the stack.
                 target_position = {k: v for k, v in self.stack_position.items()}
-                target_position["y"] = self.object_scale_factor["y"] * (self.cube_index + 1)
+                target_position["y"] = self.raycast_stack() + 0.3
 
                 # Reach for the point above the stack.
-                # `from_held` and `held_point` will offset `target_position` by the cube's top bound point.
+                # `from_held` and `held_point` will offset `target_position` by the object's top bound point.
                 # `plan` subdivides the action into horizontal and vertical components so that the hand doesn't knock over the stack.
                 self.replicant.reach_for(target=target_position,
                                          arm=Arm.right,
@@ -238,21 +250,21 @@ class StackObjects(Controller):
         # Reach above the stack.
         elif self.replicant_state == ReplicantState.reaching_above_stack:
             if self.action_ended(error_message="reach above stack"):
-                self.replicant_state = ReplicantState.dropping_cube
+                self.replicant_state = ReplicantState.dropping_object
 
-                # Get the target position for the cube.
-                cube_position = np.copy(self.object_manager.transforms[self.cubes[self.cube_index]].position)
+                # Get the target position for the object.
+                object_position = np.copy(self.object_manager.transforms[self.objects[self.object_index]].position)
                 # Set the x, z coordinates to be above the stack.
-                cube_position[0] = 0
-                cube_position[2] = 0
+                object_position[0] = 0
+                object_position[2] = 0
 
                 # Drop the object.
-                # `offset` will reposition the cube to be directly above the stack.
+                # `offset` will reposition the object to be directly above the stack.
                 self.replicant.drop(arm=Arm.right,
-                                    offset=cube_position)
-        # Drop the cube.
-        elif self.replicant_state == ReplicantState.dropping_cube:
-            if self.action_ended(error_message=f"drop {self.cubes[self.cube_index]}"):
+                                    offset=object_position)
+        # Drop the object.
+        elif self.replicant_state == ReplicantState.dropping_object:
+            if self.action_ended(error_message=f"drop {self.objects[self.object_index]}"):
                 self.replicant_state = ReplicantState.backing_away
                 self.communicate([])
 
@@ -267,34 +279,34 @@ class StackObjects(Controller):
                 y1 = self.raycast_stack()
                 if y1 > self.stack_y:
                     self.stack_y = y1
-                    self.cube_index += 1
+                    self.object_index += 1
                     # We're done!
-                    if self.cube_index >= len(self.cubes):
+                    if self.object_index >= len(self.objects):
                         return True
-                    # Start moving to the next cube.
+                    # Start moving to the next object.
                     else:
-                        self.replicant_state = ReplicantState.moving_to_cube
-                        self.start_moving_to_cube()
+                        self.replicant_state = ReplicantState.moving_to_object
+                        self.start_moving_to_object()
                 else:
                     print(f"Warning! Failed drop the object at the stack position.")
                     self.communicate([])
         return False
 
-    def start_moving_to_cube(self) -> None:
+    def start_moving_to_object(self) -> None:
         """
-        Start moving the Replicant to the next cube.
+        Start moving the Replicant to the next object.
         """
 
-        cube_position = TDWUtils.array_to_vector3(self.object_manager.transforms[self.cubes[self.cube_index]].position)
-        cube_position["y"] = 0
+        object_position = TDWUtils.array_to_vector3(self.object_manager.transforms[self.objects[self.object_index]].position)
+        object_position["y"] = 0
 
-        # Move to the cube.
-        # `arrived_at` is set to make sure that the Replicant is at a distance at which it's easy to pick up the cube.
-        self.replicant.move_to(target=self.cubes[self.cube_index], arrived_at=0.25)
+        # Move to the object.
+        # `arrived_at` is set to make sure that the Replicant is at a distance at which it's easy to pick up the object.
+        self.replicant.move_to(target=self.objects[self.object_index], arrived_at=0.25)
 
     def raycast_stack(self) -> float:
         """
-        Raycast the top of the stack of cubes.
+        Raycast the top of the stack of objects.
 
         :return: The height of the stack.
         """

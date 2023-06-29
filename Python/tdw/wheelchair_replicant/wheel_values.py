2120a0b4a1090fd0d94971afcf3f19ab659304c3
Esther Alter <alters@mit.edu>
2023-06-29 12:53:45 -0400

    wheel turn values

--- Python/tdw/wheelchair_replicant/wheel_values.py
+++ Python/tdw/wheelchair_replicant/wheel_values.py
@@ -1,3 +1,6 @@
+from tdw.tdw_utils import TDWUtils
+
+
 class WheelValues:
     """
     Wheel values for a move or turn action.
@@ -43,29 +46,52 @@ def get_default_values() -> WheelValues:
     return WheelValues(brake_at=0, brake_torque=0, left_motor_torque=0, right_motor_torque=0, steer_angle=0)
 
 
-def get_turn_values(angle: float) -> WheelValues:
+def get_turn_values(angle: float, arrived_at: float) -> WheelValues:
     """
     :param angle: The target angle in degrees.
+    :param arrived_at: The arrived-at threshold in degrees.
 
     :return: Wheel values for a turn action.
     """
 
-    brake_at = abs(angle) * 0.9
-    if abs(angle) < 5:
-        brake_torque = 5
-        outer_motor_torque = -5
-        inner_motor_torque = 10
+    brake_at = angle - arrived_at / 2
+    brake_torque = 5
+    a = abs(angle)
+    if a <= 5:
+        outer_motor_torque = 11
+        inner_motor_torque = -12
+    # Lerp between values that are known to be good at certain angles.
+    elif a <= 15:
+        t = TDWUtils.inv_lerp(a=5, b=15, v=a)
+        outer_motor_torque = TDWUtils.lerp(a=11, b=12, t=t)
+        inner_motor_torque = 1 - TDWUtils.lerp(a=-17, b=-12, t=t)
+    elif a <= 25:
+        t = TDWUtils.inv_lerp(a=15, b=25, v=a)
+        outer_motor_torque = 1 - TDWUtils.lerp(a=11, b=12, t=t)
+        inner_motor_torque = 1 - TDWUtils.lerp(a=-19, b=-17, t=t)
+    elif a <= 45:
+        t = TDWUtils.inv_lerp(a=25, b=45, v=a)
+        outer_motor_torque = 1 - TDWUtils.lerp(a=10, b=11, t=t)
+        inner_motor_torque = 1 - TDWUtils.lerp(a=-20, b=-19, t=t)
+    elif a <= 90:
+        t = TDWUtils.inv_lerp(a=45, b=90, v=a)
+        outer_motor_torque = TDWUtils.lerp(a=10, b=19, t=t)
+        inner_motor_torque = TDWUtils.lerp(a=-20, b=-18, t=t)
+    elif a <= 120:
+        t = TDWUtils.inv_lerp(a=90, b=120, v=a)
+        outer_motor_torque = TDWUtils.lerp(a=19, b=20, t=t)
+        inner_motor_torque = 1 - TDWUtils.lerp(a=-18, b=-10, t=t)
+    # Use the values for 120 degrees.
     else:
-        brake_torque = 20
-        outer_motor_torque = -20
-        inner_motor_torque = 5
+        outer_motor_torque = 20
+        inner_motor_torque = -10
+    # Map outer/inner to left/right.
     if angle > 0:
         left_motor_torque = outer_motor_torque
         right_motor_torque = inner_motor_torque
     else:
         left_motor_torque = inner_motor_torque
         right_motor_torque = outer_motor_torque
-        brake_torque *= -1
     return WheelValues(brake_at=brake_at, brake_torque=brake_torque, left_motor_torque=left_motor_torque,
                        right_motor_torque=right_motor_torque, steer_angle=angle)
 

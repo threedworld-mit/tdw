ca180f78ee8e87b128c5dd4e80df4aee4924ef4c
Esther Alter <alters@mit.edu>
2023-07-19 15:27:05 -0400

    better

--- Python/tdw/obi_data/wind_source.py
+++ Python/tdw/obi_data/wind_source.py
@@ -1,3 +1,4 @@
+from copy import copy
 from random import randint, uniform
 from typing import Tuple, List
 from tdw.tdw_utils import TDWUtils
@@ -5,10 +6,35 @@ from tdw.obi_data.fluids.fluid import Fluid
 from tdw.obi_data.fluids.emitter_shape import EmitterShape
 
 
+class _LerpableRange:
+    def __init__(self, lerp_range: Tuple[float, float], speed: float):
+        self.lerp_range: Tuple[float, float] = lerp_range
+        self.speed: float = abs(speed)
+        self._target: float = 0
+        self._v: float = 0
+        self._set_target()
+
+    def update(self) -> float:
+        if self._v < self._target:
+            self._v += self.speed
+            if self._v >= self._target:
+                self._set_target()
+        else:
+            self._v -= self.speed
+            if self._v <= self._target:
+                self._set_target()
+        return self._v
+
+    def _set_target(self) -> None:
+        self._target = TDWUtils.lerp(self.lerp_range[0], self.lerp_range[1], uniform(0, 1))
+
+
 class WindSource:
     """
     A source of wind: An invisible Obi fluid that can dynamically adjust its rotation, speed, etc.
     """
+
+    INITIAL_CAPACITY: int = 1000
 
     def __init__(self, wind_id: int, fluid: Fluid):
         """
@@ -18,34 +44,18 @@ class WindSource:
 
         self.wind_id: int = wind_id
         self.fluid: Fluid = fluid
-        self.capacities: Tuple[int, int] = (1000, 2000)
-        self._capacity_range: Tuple[int, int] = self._get_capacity_range()
-        self._capacity_t: float = 0
-        self._capacity_dt: float = 0
+        self._capacity: _LerpableRange = _LerpableRange(lerp_range=(WindSource.INITIAL_CAPACITY,
+                                                                    WindSource.INITIAL_CAPACITY * 2),
+                                                        speed=0)
 
-    def set_gustiness(self, ):
+    def set_gustiness(self, capacity_range: Tuple[int, int], speed: float) -> None:
+        self._capacity.lerp_range = capacity_range
+        self._capacity.speed = speed
 
     def update(self) -> List[dict]:
         commands = []
-        # Increment the capacity time.
-        self._capacity_t += self._capacity_dt
-        # Reverse the lerp.
-        if self._capacity_t >= 1 and self._capacity_dt > 0:
-            self._capacity_t = 1
-            self._capacity_dt *= -1
-        elif self._capacity_t <= 0 and self._capacity_dt < 0:
-            self._capacity_t = 0
-            self._capacity_dt *= -1
-        # Get the lerped capacity.
-        capacity: int = int(TDWUtils.lerp(self._capacity_range[0], self._capacity_range[1], self._capacity_t))
+        # Set the capacity.
         commands.append({"$type": "set_obi_fluid_capacity",
                          "id": self.wind_id,
-                         "capacity": capacity})
+                         "capacity": int(self._capacity.update())})
         return commands
-
-    def _get_capacity_range(self) -> Tuple[int, int]:
-        c0: float = uniform(0, 0.3)
-        c1: float = uniform(c0, 1)
-        a = int(TDWUtils.lerp(self.capacities[0], self.capacities[1], c0))
-        b = int(TDWUtils.lerp(self.capacities[0], self.capacities[1], c1))
-        return a, b

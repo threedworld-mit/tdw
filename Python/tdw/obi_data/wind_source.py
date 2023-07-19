43914b595b143a5c1ffc0af1d6232d1ffa6641f3
Esther Alter <alters@mit.edu>
2023-07-19 16:12:33 -0400

    hmm ok

--- Python/tdw/obi_data/wind_source.py
+++ Python/tdw/obi_data/wind_source.py
@@ -3,12 +3,12 @@ from random import randint, uniform
 from typing import Tuple, List
 from tdw.tdw_utils import TDWUtils
 from tdw.obi_data.fluids.fluid import Fluid
-from tdw.obi_data.fluids.emitter_shape import EmitterShape
+from tdw.obi_data.fluids.disk_emitter import DiskEmitter
 
 
 class _LerpableRange:
-    def __init__(self, lerp_range: Tuple[float, float], speed: float):
-        self.lerp_range: Tuple[float, float] = lerp_range
+    def __init__(self, value: float, speed: float):
+        self.lerp_range: Tuple[float, float] = (value, value * 2)
         self.speed: float = abs(speed)
         self._target: float = 0
         self._v: float = 0
@@ -29,33 +29,87 @@ class _LerpableRange:
         self._target = TDWUtils.lerp(self.lerp_range[0], self.lerp_range[1], uniform(0, 1))
 
 
+class _LerpableFloat:
+    def __init__(self, a: float, b: float, speed: float):
+        self._v: float = a
+        self._target: float = b
+        self._speed: float = abs(speed)
+        self._increase: bool = b > a
+
+    def update(self) -> float:
+        if self._v < self._target and self._increase:
+            self._v += self._speed
+        elif self._v > self._target and not self._increase:
+            self._v -= self._speed
+        return self._v
+
+    def set_target(self, target: float, speed: float) -> None:
+        self._target = target
+        self._speed = abs(speed)
+        self._increase = self._v < self._target
+
+
+
 class WindSource:
     """
     A source of wind: An invisible Obi fluid that can dynamically adjust its rotation, speed, etc.
     """
 
-    INITIAL_CAPACITY: int = 1000
-
-    def __init__(self, wind_id: int, fluid: Fluid):
+    def __init__(self, wind_id: int, capacity: int = 2000, speed: float = 1, lifespan: float = 0.5, smoothing: float = 0.5,
+                 resolution: float = 1, radius_scale: float = 20, vorticity: float = 10, random_velocity: float = 0.125,
+                 visible: bool = False, emitter_radius: float = 0.25):
         """
         :param wind_id: The ID of this wind source.
-        :param fluid: The wind's fluid data.
         """
 
         self.wind_id: int = wind_id
-        self.fluid: Fluid = fluid
-        self._capacity: _LerpableRange = _LerpableRange(lerp_range=(WindSource.INITIAL_CAPACITY,
-                                                                    WindSource.INITIAL_CAPACITY * 2),
-                                                        speed=0)
+        self.fluid: Fluid = Fluid(capacity=capacity,
+                                  smoothing=smoothing,
+                                  resolution=resolution,
+                                  color={"r": 0, "g": 0, "b": 1, "a": 1},
+                                  transparency=1 if visible else 0,
+                                  thickness_cutoff=1 if visible else 100,
+                                  viscosity=0,
+                                  rest_density=1.293,
+                                  atmospheric_drag=0,
+                                  atmospheric_pressure=0,
+                                  particle_z_write=False,
+                                  thickness_downsample=2,
+                                  radius_scale=radius_scale,
+                                  surface_tension=0,
+                                  vorticity=vorticity,
+                                  random_velocity=random_velocity,
+                                  buoyancy=0,
+                                  diffusion=0,
+                                  diffusion_data=None,
+                                  blur_radius=0.02,
+                                  surface_downsample=1,
+                                  render_smoothness=0.8,
+                                  metalness=0,
+                                  absorption=0,
+                                  ambient_multiplier=0,
+                                  reflection=0,
+                                  refraction=0,
+                                  foam_downsample=1)
+        self.emitter: DiskEmitter = DiskEmitter(radius=emitter_radius)
+        self._capacity: _LerpableRange = _LerpableRange(value=capacity, speed=0)
+        self._lifespan: _LerpableRange = _LerpableRange(value=lifespan, speed=0)
+        self._speed: _LerpableFloat = _LerpableFloat(a=speed, b=speed, speed=0)
 
-    def set_gustiness(self, capacity_range: Tuple[int, int], speed: float) -> None:
-        self._capacity.lerp_range = capacity_range
-        self._capacity.speed = speed
+    def set_gustiness(self, capacity: Tuple[int, int], capacity_dt: float,
+                      lifespan: Tuple[float, float], lifespan_dt: float) -> None:
+        self._capacity.lerp_range = copy(capacity)
+        self._capacity.speed = capacity_dt
+        self._lifespan.lerp_range = copy(lifespan)
+        self._lifespan.speed = lifespan_dt
+
+    def set_speed(self, target: float, speed_dt: float):
+
 
     def update(self) -> List[dict]:
-        commands = []
-        # Set the capacity.
-        commands.append({"$type": "set_obi_fluid_capacity",
-                         "id": self.wind_id,
-                         "capacity": int(self._capacity.update())})
-        return commands
+        return [{"$type": "set_obi_fluid_capacity",
+                 "id": self.wind_id,
+                 "capacity": int(self._capacity.update())},
+                {"$type": "set_obi_fluid_lifespan",
+                 "id": self.wind_id,
+                 "lifespan": self._lifespan.update()}]

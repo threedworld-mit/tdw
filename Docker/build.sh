3cb2d38254f8b1c07c5996ff2e0e916545d82cd1
Esther Alter <alters@mit.edu>
2023-09-22 12:14:44 -0400

    nvidia??

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-VERSION=$(python3 ./tdw_version.py)
+# Get the most recent version of TDW by checking the version.py script on Gitub.
+tdw_version=$(curl -s https://raw.githubusercontent.com/threedworld-mit/tdw/master/Python/tdw/version.py)
+pattern="__version__ = \"(.*?)\""
+[[ "$tdw_version" =~ $pattern ]] && tdw_version="${BASH_REMATCH[1]}"
 
- docker build -t alters/tdw:$VERSION --build-arg TDW_VERSION=$VERSION .
+# Get this computer's NVIDIA version.
+# Source: https://github.com/mviereck/x11docker/wiki/NVIDIA-driver-support-for-docker-container#nvidia-driver-base-image
+nvidia_version="$(head -n1 </proc/driver/nvidia/version | awk '{ print $8 }')"
+
+# Build the container.
+docker build -t tdw/tdw:$VERSION --build-arg TDW_VERSION=$tdw_version NVIDIA_VERSION=$nvidia_version .

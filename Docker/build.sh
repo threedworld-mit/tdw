1023b4401198923b1bd769738c78b6e16dcd1a99
Seth Alter <alters@mit.edu>
2023-09-26 09:06:09 -0400

    removed bad steps

--- Docker/build.sh
+++ Docker/build.sh
@@ -5,9 +5,5 @@ TDW_VERSION=$(curl -s https://raw.githubusercontent.com/threedworld-mit/tdw/mast
 PATTERN='__version__ = \"(.*?)\"'
 [[ "$TDW_VERSION" =~ $PATTERN ]] && TDW_VERSION="${BASH_REMATCH[1]}"
 
-# Get this computer's NVIDIA version.
-# Source: https://github.com/mviereck/x11docker/wiki/NVIDIA-driver-support-for-docker-container#nvidia-driver-base-image
-NVIDIA_VERSION="$(head -n1 </proc/driver/nvidia/version | awk '{ print $8 }')"
-
 # Build the container.
-docker build -t tdw/tdw:$TDW_VERSION --build-arg TDW_VERSION=$TDW_VERSION --build-arg NVIDIA_VERSION=$NVIDIA_VERSION .
+docker build -t tdw/tdw:$TDW_VERSION --build-arg TDW_VERSION=$TDW_VERSION .

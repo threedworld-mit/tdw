20dc23376b1ab67242c6b516681bc3db6960dedb
Seth Alter <alters@mit.edu>
2023-11-08 09:44:21 -0500

    removed print statement

--- Python/tdw/controller.py
+++ Python/tdw/controller.py
@@ -572,7 +572,6 @@ class Controller:
         parser.add_argument("--flip_images", action="store_true")
         parser.add_argument("--force_glcore42", action="store_true")
         args, unknown = parser.parse_known_args()
-        print("ARGS", args, unknown)
         build_call = [str(BUILD_PATH.resolve()), "-port " + str(port)]
         if args.flip_images:
             build_call.append("-flip_images")

c5952eb3c086fbe8fff489dacdf9391a0a9853b0
Seth Alter <alters@mit.edu>
2024-02-27 12:05:37 -0500

    minor

--- Python/tdw/dev/code_gen/cached_objects.py
+++ Python/tdw/dev/code_gen/cached_objects.py
@@ -20,34 +20,26 @@ class CachedObjects:
         """
 
         config = Config()
-        # Set the output directory.
         documentation_directory = config.tdw_docs_path.joinpath("docs/unity/cached_object_api")
         recreate_directory(documentation_directory)
         klasses: Dict[str, Klass] = {k.id: k for k in assembly.namespaces["TDW"].klasses}
         cached_object_types: List[str] = list()
+        # Get all types of CachedObject.
         for k in assembly.namespaces["TDW"].klasses:
-            if k.name == "CachedObjectBase":
-                CachedObjects.get_cached_object_types(kid=k.id, cached_object_types=cached_object_types, klasses=klasses)
-                break
-        toc = ("# Cached Object API\n\n"
-               "In TDW, Cached Objects are C# data structures that store data such as object IDs and GameObjects.\n\n"
-               "![](../images/cached_objects.png)\n\n***\n\n")
-        links = []
+            # We need to do this by name rather than class inheritance because of generic types.
+            if k.name.startswith("Cached"):
+                cached_object_types.append(k.id)
+        # Generate the overview doc.
+        toc = "# Cached Object API\n\n"
+        toc += config.tdw_docs_path.joinpath("docs/unity/cached_object_api_resources/cached_object_api_overview.md").read_text()
+        toc += "\n\n![](../cached_object_api_resources/images/cached_objects.png)\n\n***\n\n"
+        documentation_directory.joinpath("cached_object_api.md").write_text(toc)
+        # Generate the API docs.
         for co in cached_object_types:
             k = klasses[co]
             filename = underscore(k.name) + ".md"
-            links.append(f"- [{k.name}]({filename})\n")
             doc = f"# {k.name}\n\n{k.description}\n\n{k.get_cs_fields_table()}"
             documentation_directory.joinpath(filename).write_text(doc)
-        toc += "\n".join(links)
-        documentation_directory.joinpath("cached_object_api.md").write_text(toc)
-
-    @staticmethod
-    def get_cached_object_types(kid: str, cached_object_types: List[str], klasses: Dict[str, Klass]) -> None:
-        co = klasses[kid]
-        for child_id in co.child_ids:
-            cached_object_types.append(child_id)
-            CachedObjects.get_cached_object_types(child_id, cached_object_types, klasses)
 
 
 if __name__ == "__main__":

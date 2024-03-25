c9a88267e8f73a30f92f3535dc3a7ee73715cbb5
instbldrjems <jeremyes@mit.edu>
2024-03-25 15:11:10 -0400

    Update fove_human_leap_motion_minimal.py

--- Python/example_controllers/vr/fove_human_leap_motion_minimal.py
+++ Python/example_controllers/vr/fove_human_leap_motion_minimal.py
@@ -8,15 +8,14 @@ Minimal Fove Leap Motion example.
 
 c = Controller(launch_build=False)
 commands = [TDWUtils.create_empty_room(12, 12)]
-z = 0.3
 commands.extend(Controller.get_add_physics_object(model_name="small_table_green_marble",
                                                   object_id=Controller.get_unique_id(),
-                                                  position={"x": 0, "y": 0, "z": z},
+                                                  position={"x": 0, "y": 0, "z": 0},
                                                   scale_factor={"x": 1.0, "y": 1.0, "z": 1.0},
                                                   kinematic=True))
 commands.extend(Controller.get_add_physics_object(model_name="cube",
                                                   object_id=Controller.get_unique_id(),
-                                                  position={"x": 0, "y": 1, "z": z - 0.25},
+                                                  position={"x": 0, "y": 1, "z": -0.25},
                                                   scale_mass=False,
                                                   scale_factor={"x": 0.05, "y": 0.05, "z": 0.05},
                                                   default_physics_values=False,
@@ -24,7 +23,7 @@ commands.extend(Controller.get_add_physics_object(model_name="cube",
                                                   library="models_flex.json"))
 commands.extend([{"$type": "set_post_process", "value": False}, 
                  {"$type": "set_target_framerate", "framerate": -1}])
-vr = FoveHumanLeapMotion(time_step=0.02)
+vr = FoveHumanLeapMotion(position={"x": 0, "y": 0.685, "z": -1.05}, time_step=0.02)
 c.add_ons.append(vr)
 c.communicate(commands)
 while not vr.done:

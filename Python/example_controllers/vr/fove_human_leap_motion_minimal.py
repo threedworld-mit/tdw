2ca53f662e14c08eefafcc73f4a249b6620c15ea
instbldrjems <jeremyes@mit.edu>
2024-03-26 10:40:31 -0400

    Added support for optional headset position send, and general clean-up

--- Python/example_controllers/vr/fove_human_leap_motion_minimal.py
+++ Python/example_controllers/vr/fove_human_leap_motion_minimal.py
@@ -11,7 +11,7 @@ commands = [TDWUtils.create_empty_room(12, 12)]
 commands.extend(Controller.get_add_physics_object(model_name="small_table_green_marble",
                                                   object_id=Controller.get_unique_id(),
                                                   position={"x": 0, "y": 0, "z": 0},
-                                                  scale_factor={"x": 1.0, "y": 1.0, "z": 1.0},
+                                                  scale_factor={"x": 1.0, "y": 1.0, "z": 0.75},
                                                   kinematic=True))
 commands.extend(Controller.get_add_physics_object(model_name="cube",
                                                   object_id=Controller.get_unique_id(),
@@ -21,9 +21,57 @@ commands.extend(Controller.get_add_physics_object(model_name="cube",
                                                   default_physics_values=False,
                                                   mass=1,
                                                   library="models_flex.json"))
+commands.extend(Controller.get_add_physics_object(model_name="zenblocks",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0, "y": 1, "z": 0},
+                                                  scale_mass=False,
+                                                  scale_factor={"x": 0.5, "y": 0.5, "z": 0.5},
+                                                  default_physics_values=False,
+                                                  mass=1,
+                                                  library="models_core.json"))
+commands.extend(Controller.get_add_physics_object(model_name="b03_cocacola_can_cage",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0.4, "y": 1, "z": 0.01},
+                                                  scale_mass=False,
+                                                  scale_factor={"x": 1.35, "y": 1.35, "z": 1.35},
+                                                  default_physics_values=False,
+                                                  mass=1,
+                                                  library="models_full.json"))
+commands.extend(Controller.get_add_physics_object(model_name="vase_02",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": -0.3, "y": 1, "z": -0.07},
+                                                  scale_mass=False,
+                                                  scale_factor={"x": 0.85, "y": 0.85, "z": 0.85},
+                                                  default_physics_values=False,
+                                                  mass=1,
+                                                  library="models_core.json"))
+commands.extend(Controller.get_add_physics_object(model_name="champagne_cork",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0.2, "y": 1, "z": -0.15},
+                                                  scale_mass=False,
+                                                  scale_factor={"x": 1.25, "y": 1.25, "z": 1.25},
+                                                  default_physics_values=False,
+                                                  mass=1,
+                                                  library="models_core.json"))
+commands.extend(Controller.get_add_physics_object(model_name="mouse_02_vray",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": 0.4, "y": 1, "z": -0.15},
+                                                  scale_mass=False,
+                                                  scale_factor={"x": 1, "y": 1, "z": 1},
+                                                  default_physics_values=False,
+                                                  mass=1,
+                                                  library="models_core.json"))
+commands.extend(Controller.get_add_physics_object(model_name="rh10",
+                                                  object_id=Controller.get_unique_id(),
+                                                  position={"x": -0.4, "y": 1, "z": -0.15},
+                                                  scale_mass=False,
+                                                  scale_factor={"x": 0.2, "y": 0.2, "z": 0.2},
+                                                  default_physics_values=False,
+                                                  mass=1,
+                                                  library="models_core.json"))
 commands.extend([{"$type": "set_post_process", "value": False}, 
                  {"$type": "set_target_framerate", "framerate": -1}])
-vr = FoveHumanLeapMotion(position={"x": 0, "y": 0.685, "z": -1.05}, time_step=0.02)
+vr = FoveHumanLeapMotion(position={"x": 0, "y": 1.195, "z": -0.475}, time_step=0.02, use_headset_position=False)
 c.add_ons.append(vr)
 c.communicate(commands)
 while not vr.done:

406f8e6431c2a6052ce696f5ba44e7abc87cb035
jemsMIT <jschwartz@wireframe-world.com>
2024-04-16 14:23:46 -0400

    Update fove_calibration_test.py

--- Python/example_controllers/vr/fove_calibration_test.py
+++ Python/example_controllers/vr/fove_calibration_test.py
@@ -3,6 +3,7 @@ from tdw.tdw_utils import TDWUtils
 from tdw.add_ons.fove_leap_motion import FoveLeapMotion
 from tdw.output_data import OutputData, Fove
 from tdw.add_ons.object_manager import ObjectManager
+import time
 
 """
 Fove eye/hand calibration protocol.
@@ -10,13 +11,17 @@ Fove eye/hand calibration protocol.
 
 c = Controller(launch_build=False)
 commands = [TDWUtils.create_empty_room(12, 12)]
-vr = FoveLeapMotion(position={"x": 0, "y": 1, "z": 0}, time_step=0.01)
-c.add_ons.append(vr)
+fove = FoveLeapMotion(position={"x": 0, "y": 1, "z": 0}, 
+                                time_step=0.01, 
+                                calibration_data_path="C:\\Users\\weiwe\\OneDrive\\Documents\\eye_hand_data\\eye_hand_data_" + str(time.time))
+c.add_ons.append(fove)
 om = ObjectManager()
 c.add_ons.append(om)
 c.communicate(commands)
 
-while not vr.done:
+while not fove.done:
+    if fove.calibration_state == CalibrationState.running:
+        fove.initialize_scene()
     c.communicate([])
    
 c.communicate({"$type": "terminate"})
